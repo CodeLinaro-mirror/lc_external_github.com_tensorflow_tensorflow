a5c2ba5f2c7baf061eb98e76e89de21b97a29a3d
Subhankar Shah <subhankarshah@google.com>
2025-07-17 17:54:41 -0700

    [XLA:MSA] Reduce available memory bandwidth for instruction that are overlapped with bandwidth limiting asynchronous instructions.
    PiperOrigin-RevId: 784379357

--- third_party/xla/xla/service/memory_space_assignment/options.h
+++ third_party/xla/xla/service/memory_space_assignment/options.h
@@ -78,6 +78,8 @@ using DetermineSplitDimensionFunction =
 using BitcastSplitFn = std::function<absl::StatusOr<int64_t>(
     const HloInstruction* instruction, int64_t split_dim)>;
 using ShapeSizeFn = std::function<int64_t(const Shape&)>;
+using AsyncInstructionBwAdjustmentFactorFn =
+    std::function<std::optional<float>(const HloInstruction*)>;
 using HloPositionOrUse = std::variant<HloPosition, HloUse>;
 
 // MSA allows for custom post-allocation transformations. When a post-allocation
@@ -393,6 +395,15 @@ struct Options {
   // If set, this is the size of scoped alternate memory that we require MSA to
   // allocate for post-module operations.
   uint64_t post_module_scoped_alternate_memory_size_in_bytes = 0;
+
+  // Determines the bandwidth adjustment factor for an async start instruction.
+  // The available bandwidth for instructions between this and the async done
+  // instruction will be multiplied by the factor returned by this function. A
+  // factor of 1.0 means that the full bandwidth is available. A factor of 0.5
+  // means that only half the bandwidth is available.
+  AsyncInstructionBwAdjustmentFactorFn
+      async_instruction_bw_adjustment_factor_fn =
+          [](const HloInstruction*) { return std::nullopt; };
 
   std::string ToString() const;
 };
