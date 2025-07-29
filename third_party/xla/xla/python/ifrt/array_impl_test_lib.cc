2ce2b03f090258ecf264db2694205e4cf82f92c9
Hyeontaek Lim <hyeontaek@google.com>
2025-07-29 14:54:02 -0700

    [IFRT] Remove an explicit `user_context` argument from APIs
    `Client::MakeArrayFromHostBuffer`, `Client::MakeArraysFromHostBufferShards`,
    and `Client::MakeErrorArrays` took explicit `user_context` argument to
    associate a user context for arrays created by these APIs.
    
    This path is migrated to the `UserContextScope` mechanism where
    `UserContextScope` is set up before calling such APIs. This gives a more
    uniform way of specifying active `UserContext` for IFRT API calls and simplfies
    the path to expanding the use of `UserContext` to all array returning APIs.
    
    PiperOrigin-RevId: 788618421

--- third_party/xla/xla/python/ifrt/array_impl_test_lib.cc
+++ third_party/xla/xla/python/ifrt/array_impl_test_lib.cc
@@ -453,8 +453,7 @@ TEST(ArrayImplTest, MakeArraysFromHostBufferShardsAndCopyToHostBuffer) {
   TF_ASSERT_OK_AND_ASSIGN(
       auto arrays, client->MakeArraysFromHostBufferShards(
                        absl::MakeSpan(specs),
-                       Client::HostBufferSemantics::kImmutableOnlyDuringCall,
-                       client->CreateUserContext()));
+                       Client::HostBufferSemantics::kImmutableOnlyDuringCall));
   ASSERT_THAT(arrays, SizeIs(2));
 
   // Once the `Array` has become ready, the host buffer is not accessed.
@@ -527,8 +526,7 @@ TEST(ArrayImplTest, MakeArraysFromHostBufferShardsWithDifferentDevices) {
   absl::Status status;
   auto result = client->MakeArraysFromHostBufferShards(
       absl::MakeSpan(specs),
-      Client::HostBufferSemantics::kImmutableOnlyDuringCall,
-      client->CreateUserContext());
+      Client::HostBufferSemantics::kImmutableOnlyDuringCall);
   if (result.ok()) {
     // Implementations may poison outputs instead of immediately returning an
     // error.
@@ -583,8 +581,7 @@ TEST(ArrayImplTest, MakeArraysFromHostBufferShardsWithDifferentMemoryKinds) {
   absl::Status status;
   auto result = client->MakeArraysFromHostBufferShards(
       absl::MakeSpan(specs),
-      Client::HostBufferSemantics::kImmutableOnlyDuringCall,
-      client->CreateUserContext());
+      Client::HostBufferSemantics::kImmutableOnlyDuringCall);
   if (result.ok()) {
     // Implementations may poison outputs instead of immediately returning an
     // error.
@@ -698,8 +695,7 @@ TEST(ArrayImplTest,
       auto arrays,
       client->MakeArraysFromHostBufferShards(
           absl::MakeSpan(specs),
-          Client::HostBufferSemantics::kImmutableUntilTransferCompletes,
-          client->CreateUserContext()));
+          Client::HostBufferSemantics::kImmutableUntilTransferCompletes));
   ASSERT_THAT(arrays, SizeIs(2));
 
   // Resetting these references does not necessarily destroy host buffers
@@ -766,8 +762,7 @@ TEST(ArrayImplTest, MakeErrorArrays) {
   const absl::Status error = absl::InternalError("injected error");
   TF_ASSERT_OK_AND_ASSIGN(
       const std::vector<xla::ifrt::ArrayRef> arrays,
-      client->MakeErrorArrays(error, {array_spec, array_spec},
-                              client->CreateUserContext()));
+      client->MakeErrorArrays(error, {array_spec, array_spec}));
   ASSERT_EQ(arrays.size(), 2);
 
   EXPECT_THAT(arrays[0]->GetReadyFuture().Await(),
@@ -803,8 +798,7 @@ TEST(ArrayImplTest, MakeErrorArraysWithAddressableAndNonAddressableDevice) {
   const absl::Status error = absl::InternalError("injected error");
   TF_ASSERT_OK_AND_ASSIGN(
       const std::vector<xla::ifrt::ArrayRef> arrays,
-      client->MakeErrorArrays(error, {array_spec, array_spec},
-                              client->CreateUserContext()));
+      client->MakeErrorArrays(error, {array_spec, array_spec}));
   ASSERT_EQ(arrays.size(), 2);
 
   EXPECT_THAT(arrays[0]->GetReadyFuture().Await(),
