e0417f7e04bd305ecef296ba38e63f50f75b9ab2
Dirk Hornung <dirkh@google.com>
2025-08-06 08:05:17 -0700

    [XLA:GPU] Add autotuning cache to Autotuner, AutotunerPass and autotuner_main.
    PiperOrigin-RevId: 791694459

--- third_party/xla/xla/backends/autotuner/autotuner.h
+++ third_party/xla/xla/backends/autotuner/autotuner.h
@@ -24,7 +24,7 @@ limitations under the License.
 #include "absl/functional/function_ref.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
-#include "xla/backends/autotuner/autotune_config.h"
+#include "xla/backends/autotuner/autotuner_cache_interface.h"
 #include "xla/backends/autotuner/codegen_backend.h"
 #include "xla/backends/autotuner/profiler.h"
 #include "xla/hlo/ir/hlo_instruction.h"
@@ -37,11 +37,24 @@ using InstructionFilterFn = absl::FunctionRef<bool(const xla::HloInstruction&)>;
 
 namespace xla {
 
+struct AutotuneConfig {
+  // Whether to skip configs that failed to compile.
+  bool skip_failing_configs = true;
+  // Whether to check the correctness of the output buffers and OOM reads on
+  // Input Buffers.
+  bool check_buffers = false;
+  // Relative tolerance for correctness check.
+  float relative_tolerance = 1e-6;
+  // Whether to crash the process on check failure.
+  bool crash_on_check_failure = false;
+};
+
 class Autotuner {
  public:
   static absl::StatusOr<std::unique_ptr<Autotuner>> Create(
       std::vector<std::unique_ptr<CodegenBackend>> codegen_backends,
       std::unique_ptr<Profiler> profiler, AutotuneConfig autotune_config,
+      std::unique_ptr<AutotunerCacheInterface> cache,
       tsl::thread::ThreadPool* thread_pool = nullptr);
 
   // Try all supported configs from the registered codegen backends for the
@@ -70,10 +83,12 @@ class Autotuner {
 
   Autotuner(std::vector<std::unique_ptr<CodegenBackend>> codegen_backends,
             std::unique_ptr<Profiler> profiler, AutotuneConfig autotune_config,
+            std::unique_ptr<AutotunerCacheInterface> cache,
             tsl::thread::ThreadPool* thread_pool)
       : codegen_backends_(std::move(codegen_backends)),
         profiler_(std::move(profiler)),
         autotune_config_(autotune_config),
+        cache_(std::move(cache)),
         thread_pool_(thread_pool) {}
 
   InstructionsByFingerprint GetAutotuningCandidates(
@@ -87,7 +102,7 @@ class Autotuner {
       HloInstruction* instr, std::vector<Config>& configs);
 
   absl::StatusOr<Config> ProfileAndPickBest(
-      std::vector<ExecutableCandidate>& candidates);
+      HloInstruction* instr, std::vector<ExecutableCandidate>& candidates);
 
   absl::StatusOr<ScopedShapedBuffer> GetReferenceOutput(
       std::vector<ExecutableCandidate>& candidates,
@@ -100,6 +115,7 @@ class Autotuner {
   std::vector<std::unique_ptr<CodegenBackend>> codegen_backends_;
   std::unique_ptr<Profiler> profiler_;
   AutotuneConfig autotune_config_;
+  std::unique_ptr<AutotunerCacheInterface> cache_;
   tsl::thread::ThreadPool* thread_pool_;
 };
 }  // namespace xla
--- third_party/xla/xla/service/gpu/autotuning/autotuner_pass.cc
+++ third_party/xla/xla/service/gpu/autotuning/autotuner_pass.cc
@@ -16,6 +16,7 @@ limitations under the License.
 #include "xla/service/gpu/autotuning/autotuner_pass.h"
 
 #include <memory>
+#include <string>
 #include <utility>
 #include <vector>
 
@@ -26,7 +27,9 @@ limitations under the License.
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "xla/backends/autotuner/autotuner.h"
+#include "xla/backends/autotuner/autotuner_cache_interface.h"
 #include "xla/backends/autotuner/codegen_backend.h"
+#include "xla/backends/autotuner/file_based_autotuner_cache.h"
 #include "xla/backends/autotuner/profiler.h"
 #include "xla/backends/gpu/autotuner/gpu_profiler.h"
 #include "xla/hlo/ir/hlo_instruction.h"
@@ -38,19 +41,52 @@ limitations under the License.
 #include "xla/tsl/platform/statusor.h"
 #include "xla/tsl/platform/threadpool.h"
 
+constexpr char kAutotunerCacheVersion[] = "1";
+
 namespace xla {
 namespace gpu {
 
 absl::StatusOr<std::unique_ptr<AutotunerPass>> AutotunerPass::Create(
     std::vector<std::unique_ptr<CodegenBackend>> backends,
+    const DebugOptions& debug_options,
     stream_executor::StreamExecutor* stream_executor,
     tsl::thread::ThreadPool* thread_pool) {
   std::unique_ptr<GpuProfiler> profiler =
       GpuProfiler::Create(stream_executor, ProfileOptions());
-  TF_ASSIGN_OR_RETURN(
-      std::unique_ptr<Autotuner> autotuner,
-      Autotuner::Create(std::move(backends), std::move(profiler),
-                        AutotuneConfig(), thread_pool));
+
+  std::unique_ptr<AutotunerCacheInterface> cache_manager = nullptr;
+  const std::string& cache_dir =
+      debug_options.xla_gpu_per_fusion_autotune_cache_dir();
+  if (!cache_dir.empty()) {
+    FileBasedCacheConfig cache_config;
+    cache_config.autotune_cache_dir = cache_dir;
+    switch (debug_options.xla_gpu_experimental_autotune_cache_mode()) {
+      case DebugOptions::AUTOTUNE_CACHE_MODE_READ:
+        cache_config.autotune_cache_mode =
+            FileBasedCacheConfig::CacheMode::READ;
+        break;
+      case DebugOptions::AUTOTUNE_CACHE_MODE_UPDATE:
+        cache_config.autotune_cache_mode =
+            FileBasedCacheConfig::CacheMode::READ_WRITE;
+        break;
+      default:
+        // Includes AUTOTUNE_CACHE_MODE_UNSPECIFIED
+        LOG(WARNING) << "Unknown autotune cache mode, defaulting to READ_WRITE";
+        cache_config.autotune_cache_mode =
+            FileBasedCacheConfig::CacheMode::READ_WRITE;
+        break;
+    }
+    TF_ASSIGN_OR_RETURN(
+        cache_manager,
+        FileBasedAutotunerCache::Create(cache_config,
+                                        stream_executor->GetDeviceDescription(),
+                                        kAutotunerCacheVersion));
+  }
+
+  TF_ASSIGN_OR_RETURN(std::unique_ptr<Autotuner> autotuner,
+                      Autotuner::Create(std::move(backends),
+                                        std::move(profiler), AutotuneConfig(),
+                                        std::move(cache_manager), thread_pool));
   return absl::WrapUnique(new AutotunerPass(std::move(autotuner)));
 }
 
