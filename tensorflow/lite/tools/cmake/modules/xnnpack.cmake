c0380567e5e620c1a73fc751d0f582676034aca4
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-11 11:40:53 -0700

    Update XNNPACk in tensorflow and XLA to get new thread pool APIs
    PiperOrigin-RevId: 793736259

--- tensorflow/lite/tools/cmake/modules/xnnpack.cmake
+++ tensorflow/lite/tools/cmake/modules/xnnpack.cmake
@@ -23,7 +23,7 @@ OverridableFetchContent_Declare(
   xnnpack
   GIT_REPOSITORY https://github.com/google/XNNPACK
   # Sync with tensorflow/workspace2.bzl
-  GIT_TAG f362d32fa5f8c8f36da487e13f5b59c911ce6b22
+  GIT_TAG a93594d002c551150503e6307f615bf97fed776f
   GIT_PROGRESS TRUE
   PREFIX "${CMAKE_BINARY_DIR}"
   SOURCE_DIR "${CMAKE_BINARY_DIR}/xnnpack"
