1a0128ed1a3bfc5ba4983abfd0652585db0325cf
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-12 10:09:27 -0700

    Update the `coords` of `PjRtStreamExecutorDeviceDescription`.
    The `coords` is updated from `[local_device_id, process_index, partition_index]` to `[partition_index, process_index_in_partition, local_device_id]`
    
    1. The new `coords` contains the same information, but the new dense assignment is much easier for read & use. e.g. users can now easily use a for-loop to iterate through coords of the topology.
    2. The 2nd dimension of `coords` is updated from "process_index within the topology" to "process_index within the partition" (starting from 0).
    
    PiperOrigin-RevId: 794159817

--- third_party/xla/xla/pjrt/gpu/se_gpu_pjrt_client_test.cc
+++ third_party/xla/xla/pjrt/gpu/se_gpu_pjrt_client_test.cc
@@ -108,6 +108,7 @@ limitations under the License.
 namespace xla {
 namespace {
 
+using ::testing::ElementsAre;
 using ::testing::ElementsAreArray;
 using ::testing::Eq;
 using ::testing::FloatEq;
@@ -115,8 +116,6 @@ using ::testing::Ge;
 using ::testing::Gt;
 using ::testing::HasSubstr;
 using ::testing::SizeIs;
-using ::tsl::testing::IsOkAndHolds;
-using ::tsl::testing::StatusIs;
 
 absl::StatusOr<std::unique_ptr<xla::PjRtLoadedExecutable>> CompileExecutable(
     absl::string_view program, xla::PjRtClient& client,
@@ -1278,11 +1277,11 @@ TEST(StreamExecutorGpuClientTest, GpuDeviceDescriptionTest) {
                           GetStreamExecutorGpuClient(DefaultOptions()));
   for (int device_index = 0; device_index < client->device_count();
        device_index++) {
-    auto coords =
-        static_cast<PjRtStreamExecutorDevice*>(client->devices()[device_index])
-            ->description()
-            .coords();
-    EXPECT_EQ(coords[0], device_index);
+    auto device =
+        static_cast<PjRtStreamExecutorDevice*>(client->devices()[device_index]);
+    auto coords = device->description().coords();
+    // All devices are in the same partition & process.
+    EXPECT_THAT(coords, ElementsAre(0, 0, device->local_device_id().value()));
   }
 }
 
--- third_party/xla/xla/pjrt/gpu/se_gpu_topology_description_test.cc
+++ third_party/xla/xla/pjrt/gpu/se_gpu_topology_description_test.cc
@@ -31,14 +31,14 @@ namespace {
 using ::testing::ElementsAre;
 
 void CheckDeviceDescription(const PjRtDeviceDescription& device_desc,
-                            int global_device_id, int local_device_id,
-                            int process_index, int partition_index) {
+                            int global_device_id, int process_index,
+                            const std::vector<int>& coords) {
   EXPECT_EQ(device_desc.id(), global_device_id);
   EXPECT_EQ(device_desc.process_index(), process_index);
   const auto& gpu_device_desc =
       dynamic_cast<const PjRtStreamExecutorDeviceDescription&>(device_desc);
   EXPECT_THAT(gpu_device_desc.coords(),
-              ElementsAre(local_device_id, process_index, partition_index));
+              ElementsAre(coords[0], coords[1], coords[2]));
 }
 
 TEST(StreamExecutorGpuTopologyDescriptionTest, SymmetricTopology) {
@@ -56,14 +56,14 @@ TEST(StreamExecutorGpuTopologyDescriptionTest, SymmetricTopology) {
 
   const auto device_descs = topology_desc.DeviceDescriptions();
   EXPECT_EQ(device_descs.size(), 8);
-  CheckDeviceDescription(*device_descs[0], 0, 0, 0, 0);
-  CheckDeviceDescription(*device_descs[1], 1, 1, 0, 0);
-  CheckDeviceDescription(*device_descs[2], 2, 0, 1, 0);
-  CheckDeviceDescription(*device_descs[3], 3, 1, 1, 0);
-  CheckDeviceDescription(*device_descs[4], 4, 0, 2, 1);
-  CheckDeviceDescription(*device_descs[5], 5, 1, 2, 1);
-  CheckDeviceDescription(*device_descs[6], 6, 0, 3, 1);
-  CheckDeviceDescription(*device_descs[7], 7, 1, 3, 1);
+  CheckDeviceDescription(*device_descs[0], 0, 0, {0, 0, 0});
+  CheckDeviceDescription(*device_descs[1], 1, 0, {0, 0, 1});
+  CheckDeviceDescription(*device_descs[2], 2, 1, {0, 1, 0});
+  CheckDeviceDescription(*device_descs[3], 3, 1, {0, 1, 1});
+  CheckDeviceDescription(*device_descs[4], 4, 2, {1, 0, 0});
+  CheckDeviceDescription(*device_descs[5], 5, 2, {1, 0, 1});
+  CheckDeviceDescription(*device_descs[6], 6, 3, {1, 1, 0});
+  CheckDeviceDescription(*device_descs[7], 7, 3, {1, 1, 1});
 }
 
 TEST(StreamExecutorGpuTopologyDescriptionTest, AsymmetricTopology) {
