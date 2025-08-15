3d9f4e7ca6e737c35cb8681dfb49e334fd27a05f
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-15 14:36:25 -0700

    Update `rules_ml_toolchain` version to introduce new CUDA13 repositories.
    PiperOrigin-RevId: 795616787

--- third_party/xla/build_tools/ci/build.py
+++ third_party/xla/build_tools/ci/build.py
@@ -278,7 +278,6 @@ def nvidia_gpu_build_with_compute_capability(
       ),
       options={
           "run_under": "//build_tools/ci:parallel_gpu_execute",
-          "@cuda_driver//:enable_forward_compatibility": "true",
           "//xla/tsl:ci_build": True,
           **_DEFAULT_BAZEL_OPTIONS,
       },
@@ -415,7 +414,6 @@ Build(
     ),
     options={
         "run_under": "//build_tools/ci:parallel_gpu_execute",
-        "@cuda_driver//:enable_forward_compatibility": "false",
         "//xla/tsl:ci_build": True,
         **_DEFAULT_BAZEL_OPTIONS,
     },
@@ -448,7 +446,6 @@ Build(
     ),
     options={
         "run_under": "//build_tools/ci:parallel_gpu_execute",
-        "@cuda_driver//:enable_forward_compatibility": "false",
         "//xla/tsl:ci_build": True,
         **_DEFAULT_BAZEL_OPTIONS,
     },
@@ -482,7 +479,6 @@ Build(
     options={
         "run_under": "//build_tools/ci:parallel_gpu_execute",
         # Use User Mode and Kernel Mode Drivers pre-installed on the system.
-        "@cuda_driver//:enable_forward_compatibility": "true",
         "//xla/tsl:ci_build": True,
         **_DEFAULT_BAZEL_OPTIONS,
     },
