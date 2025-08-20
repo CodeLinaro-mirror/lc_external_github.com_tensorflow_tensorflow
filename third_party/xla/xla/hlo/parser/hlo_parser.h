b8e76e44772e0e140d778ce1c80cd76e6eb09b94
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-19 17:41:54 -0700

    Add a command line benchmark tool to benchmark dot shapes specified on the command line.
    PiperOrigin-RevId: 797087733

--- third_party/xla/xla/hlo/parser/hlo_parser.h
+++ third_party/xla/xla/hlo/parser/hlo_parser.h
@@ -120,6 +120,9 @@ absl::StatusOr<PaddingConfig> ParsePaddingConfig(absl::string_view str);
 // Parses and returns a Shape::ToString-format string.
 absl::StatusOr<Shape> ParseShape(absl::string_view str);
 
+// Parses and returns a Shape::ToString-format string.
+absl::StatusOr<std::vector<Shape>> ParseShapeList(absl::string_view str);
+
 // Parses and returns a Layout::ToString-format string.
 absl::StatusOr<Layout> ParseLayout(absl::string_view str);
 
