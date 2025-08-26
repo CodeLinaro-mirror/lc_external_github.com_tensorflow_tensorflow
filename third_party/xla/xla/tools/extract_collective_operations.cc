04ed8a164d97e161ed70e13d9a32743bcaed9fef
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-26 06:44:06 -0700

    [XLA:GPU] Add reduce-scatter and allow to return the last result.
    PiperOrigin-RevId: 799542511

--- third_party/xla/xla/tools/extract_collective_operations.cc
+++ third_party/xla/xla/tools/extract_collective_operations.cc
@@ -39,12 +39,13 @@ limitations under the License.
 
 namespace {
 const char* const kUsage = R"(
-This tool extracts collective operations (all-reduce and all-gather) from HLO module and saves them together
+This tool extracts collective operations from HLO module and saves them together
 to the separate module.
 
 Usage:
 bazel run extract_collective_operations -- --input=path/to/hlo_module
-  --output=path/to/hlo_module --operations=all-reduce,all-gather
+  --output=path/to/hlo_module --operations=all-reduce,all-gather,reduce-scatter
+  --return_tuple=false
 )";
 }  // namespace
 
@@ -52,7 +53,7 @@ namespace xla {
 
 absl::Status ExtractCollectiveOperations(
     const std::string& input, const std::string& output,
-    const absl::flat_hash_set<HloOpcode>& operation_types) {
+    const absl::flat_hash_set<HloOpcode>& operation_types, bool return_tuple) {
   TF_ASSIGN_OR_RETURN(
       std::unique_ptr<HloModule> test_module,
       LoadModuleFromFile(input, std::string(tsl::io::Extension(input)),
@@ -83,6 +84,11 @@ absl::Status ExtractCollectiveOperations(
                            HloOpcode::kAllGatherDone>(instr)) {
         collective_instructions.push_back(instr);
       }
+
+      if (operation_types.contains(HloOpcode::kReduceScatter) &&
+          HloPredicateIsOp<HloOpcode::kReduceScatter>(instr)) {
+        collective_instructions.push_back(instr);
+      }
     }
   }
 
@@ -90,7 +96,7 @@ absl::Status ExtractCollectiveOperations(
     return absl::InternalError("No collective instructions found.");
   }
   auto collectives_module = ExtractCollectiveOperationsIntoNewModule(
-      collective_instructions, done_ops, non_optimized_ops);
+      collective_instructions, done_ops, non_optimized_ops, return_tuple);
 
   QCHECK_OK(tsl::WriteStringToFile(tsl::Env::Default(), output,
                                    collectives_module->ToString()))
@@ -103,11 +109,14 @@ int main(int argc, char** argv) {
   std::string input;
   std::string output;
   std::string operations;
+  bool return_tuple;
   std::vector<tsl::Flag> flag_list = {
       tsl::Flag("input", &input, "input file"),
       tsl::Flag("output", &output, "output file"),
       tsl::Flag("operations", &operations,
-                "operations. possible values: all-reduce, all-gather")};
+                "operations. possible values: all-reduce, all-gather"),
+      tsl::Flag("return_tuple", &return_tuple,
+                "return collectives results as tuple?")};
   xla::AppendDebugOptionsFlags(&flag_list);
   const std::string kUsageString =
       absl::StrCat(kUsage, "\n\n", tsl::Flags::Usage(argv[0], flag_list));
@@ -124,6 +133,10 @@ int main(int argc, char** argv) {
   if (absl::StrContains(operations, "all-gather")) {
     operation_types.insert(xla::HloOpcode::kAllGather);
   }
-  TF_CHECK_OK(xla::ExtractCollectiveOperations(input, output, operation_types));
+  if (absl::StrContains(operations, "reduce-scatter")) {
+    operation_types.insert(xla::HloOpcode::kReduceScatter);
+  }
+  TF_CHECK_OK(xla::ExtractCollectiveOperations(input, output, operation_types,
+                                               return_tuple));
   return 0;
 }
