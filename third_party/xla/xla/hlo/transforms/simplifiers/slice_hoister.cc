0c62ab22b608ab8e8016016b88be72fd5bd6a0f9
A. Unique TensorFlower <gardener@tensorflow.org>
2025-09-07 23:12:38 -0700

    [XLA] Hoist slices through transpose operations.
    PiperOrigin-RevId: 804280818

--- third_party/xla/xla/hlo/transforms/simplifiers/slice_hoister.cc
+++ third_party/xla/xla/hlo/transforms/simplifiers/slice_hoister.cc
@@ -15,6 +15,7 @@ limitations under the License.
 
 #include "xla/hlo/transforms/simplifiers/slice_hoister.h"
 
+#include <cstdint>
 #include <vector>
 
 #include "absl/container/flat_hash_set.h"
@@ -25,6 +26,9 @@ limitations under the License.
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/hlo/ir/hlo_opcode.h"
+#include "xla/permutation_util.h"
+#include "xla/shape.h"
+#include "xla/shape_util.h"
 #include "xla/tsl/platform/errors.h"
 #include "xla/tsl/platform/statusor.h"
 
@@ -32,21 +36,67 @@ namespace xla {
 
 namespace {
 
-// Helper function to attempt hoisting a slice through an add operation.
+// Helper function to attempt hoisting a slice through a transpose operation.
 // Returns true if a change was made.
-absl::StatusOr<bool> TryHoistSliceThroughAdd(HloInstruction* instruction,
-                                             HloComputation* computation) {
+absl::StatusOr<bool> TryHoistSliceThroughTranspose(
+    HloInstruction* instruction, HloComputation* computation) {
   if (instruction->opcode() != HloOpcode::kSlice) {
     return false;
   }
-  HloInstruction* operand = instruction->mutable_operand(0);
-  if (operand->opcode() != HloOpcode::kAdd) {
+  HloInstruction* transpose = instruction->mutable_operand(0);
+  if (transpose->opcode() != HloOpcode::kTranspose) {
+    return false;
+  }
+  if (instruction->shape().element_type() !=
+      transpose->shape().element_type()) {
+    VLOG(1) << "Slice element type does not match transpose element type: "
+            << instruction->shape().element_type() << " and "
+            << transpose->shape().element_type();
     return false;
   }
 
-  HloInstruction* add = operand;
-  HloInstruction* lhs = add->mutable_operand(0);
-  HloInstruction* rhs = add->mutable_operand(1);
+  // All checks passed, perform the hoisting.
+  auto dimensions_permutation = transpose->dimensions();
+  auto inversed_dimensions_permutation =
+      InversePermutation(dimensions_permutation);
+
+  std::vector<int64_t> new_slice_starts =
+      Permute(instruction->slice_starts(), inversed_dimensions_permutation);
+  std::vector<int64_t> new_slice_limits =
+      Permute(instruction->slice_limits(), inversed_dimensions_permutation);
+  std::vector<int64_t> new_slice_strides =
+      Permute(instruction->slice_strides(), inversed_dimensions_permutation);
+
+  HloInstruction* transpose_operand = transpose->mutable_operand(0);
+  Shape new_slice_shape = ShapeUtil::PermuteDimensions(
+      inversed_dimensions_permutation, instruction->shape());
+
+  HloInstruction* new_slice =
+      computation->AddInstruction(HloInstruction::CreateSlice(
+          new_slice_shape, transpose_operand, new_slice_starts,
+          new_slice_limits, new_slice_strides));
+  HloInstruction* new_transpose =
+      computation->AddInstruction(HloInstruction::CreateTranspose(
+          instruction->shape(), new_slice, dimensions_permutation));
+  TF_RETURN_IF_ERROR(
+      computation->ReplaceInstruction(instruction, new_transpose));
+  return true;
+}
+
+// Helper function to attempt hoisting a slice through an element-wise binary
+// operation. Returns true if a change was made.
+absl::StatusOr<bool> TryHoistSliceThroughElementwiseBinaryOperation(
+    HloInstruction* instruction, HloComputation* computation) {
+  if (instruction->opcode() != HloOpcode::kSlice) {
+    return false;
+  }
+  HloInstruction* slice_operand = instruction->mutable_operand(0);
+  if (!slice_operand->IsElementwiseBinary()) {
+    return false;
+  }
+
+  HloInstruction* lhs = slice_operand->mutable_operand(0);
+  HloInstruction* rhs = slice_operand->mutable_operand(1);
 
   if (lhs->shape() != rhs->shape()) {
     VLOG(1) << " Operand shapes do not match: " << lhs->shape() << " and "
@@ -59,10 +109,11 @@ absl::StatusOr<bool> TryHoistSliceThroughAdd(HloInstruction* instruction,
             << instruction->shape().element_type();
     return false;
   }
-  if (instruction->shape().element_type() != add->shape().element_type()) {
-    VLOG(1) << " Slice element type does not match add element type: "
+  if (instruction->shape().element_type() !=
+      slice_operand->shape().element_type()) {
+    VLOG(1) << " Slice element type does not match slice operand element type: "
             << instruction->shape().element_type() << " and "
-            << add->shape().element_type();
+            << slice_operand->shape().element_type();
     return false;
   }
 
@@ -76,27 +127,42 @@ absl::StatusOr<bool> TryHoistSliceThroughAdd(HloInstruction* instruction,
           instruction->shape(), rhs, instruction->slice_starts(),
           instruction->slice_limits(), instruction->slice_strides()));
   TF_RETURN_IF_ERROR(computation->ReplaceWithNewInstruction(
-      instruction,
-      HloInstruction::CreateBinary(instruction->shape(), HloOpcode::kAdd,
-                                   lhs_slice, rhs_slice)));
+      instruction, HloInstruction::CreateBinary(instruction->shape(),
+                                                slice_operand->opcode(),
+                                                lhs_slice, rhs_slice)));
   return true;
+}
+
+// Helper function that goes through a list of potential hoisting scenarios.
+// Returns true if a change was made.
+absl::StatusOr<bool> TryHoistingSlice(HloInstruction* instruction,
+                                      HloComputation* computation) {
+  auto hoisting_functions = {TryHoistSliceThroughElementwiseBinaryOperation,
+                             TryHoistSliceThroughTranspose};
+  for (auto hoisting_function : hoisting_functions) {
+    bool changed = false;
+    TF_ASSIGN_OR_RETURN(changed, hoisting_function(instruction, computation));
+    if (changed) {
+      return true;
+    }
+  }
+  return false;
 }
 
 // As slices reduce the size of the input, it can be beneficial to hoist
 // slices as high in the graph as possible, ideally right after parameter
 // reads, which could reduce both compute and memory costs.
 //
-// Currently, this pass hoists slice operations through add operations.
-// Note that this pass can create redundant slices, which can be removed by
-// running CSE.
+// Currently, this pass hoists slice operations through element-wise binary
+// operations. Note that this pass can create redundant slices, which can be
+// removed by running CSE.
 //
 // Note that algebraic simplifier also has `HandleSlice` function.
 absl::StatusOr<bool> HoistSliceOperations(HloComputation* computation) {
   bool changed = false;
   bool changed_on_last_iteration = false;
-  // TODO(b/434724820): Generalize to element-wise operations.
-  // TODO(b/434724820): Consider also other operations like broadcast, reduce,
-  // transpose, etc.
+  // TODO(b/434724820): Consider also other operations that aren't handled in
+  // algebraic simplifier.
   // TODO(b/434724820): Make this more efficient by e.g. using a worklist or a
   // topological sort.
   do {
@@ -106,7 +172,7 @@ absl::StatusOr<bool> HoistSliceOperations(HloComputation* computation) {
         computation->MakeInstructionPostOrder();
     for (HloInstruction* instruction : instructions) {
       TF_ASSIGN_OR_RETURN(bool instruction_changed,
-                          TryHoistSliceThroughAdd(instruction, computation));
+                          TryHoistingSlice(instruction, computation));
       if (instruction_changed) {
         changed_on_last_iteration = true;
         break;
