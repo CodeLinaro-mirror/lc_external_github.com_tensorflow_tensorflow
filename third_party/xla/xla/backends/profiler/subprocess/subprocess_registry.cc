hexagon-geo-surv/tensorflow

Add a profiler for subprocesses.
Reverts 68d2d01046714cf82ea03c25d1edadf40f29d7c1

PiperOrigin-RevId: 786906048

third_party/xla/xla/backends/profiler/cpu/subprocess_registry.cc
@@ -1,0 +1,120 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "xla/backends/profiler/cpu/subprocess_registry.h"
+
+#include <cstdint>
+#include <limits>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "absl/base/no_destructor.h"
+#include "absl/base/thread_annotations.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/log/log.h"
+#include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/string_view.h"
+#include "absl/synchronization/mutex.h"
+#include "grpcpp/grpcpp.h"
+#include "grpcpp/security/credentials.h"
+#include "grpcpp/support/channel_arguments.h"
+#include "xla/tsl/platform/env.h"
+#include "tsl/profiler/protobuf/profiler_service.grpc.pb.h"
+
+namespace xla {
+namespace profiler {
+namespace cpu {
+namespace {
+
+// Registry of subprocesses.
+struct Registry {
+  absl::Mutex mu;
+  absl::flat_hash_set<SubprocessInfo> subprocesses ABSL_GUARDED_BY(mu);
+};
+
+// Global registry of subprocesses.
+Registry& registry() {
+  static absl::NoDestructor<Registry> registry;
+  return *registry;
+}
+
+}  // namespace
+
+std::string SubprocessInfo::DebugString() const {
+  return absl::StrCat("SubprocessInfo(pid: ", pid, ", address: ", address, ")");
+}
+
+absl::Status RegisterSubprocess(SubprocessInfo&& subprocess_info) {
+  // This check ensures that there can't be a loop in the subprocess profiler
+  // graph.
+  if (subprocess_info.pid == tsl::Env::Default()->GetProcessId()) {
+    return absl::InvalidArgumentError(
+        "Cannot register subprocess with the same pid as current process.");
+  }
+  ::grpc::ChannelArguments channel_args;
+  channel_args.SetMaxReceiveMessageSize(std::numeric_limits<int32_t>::max());
+  auto channel = ::grpc::CreateCustomChannel(
+      subprocess_info.address, ::grpc::InsecureChannelCredentials(),
+      channel_args);
+  if (!channel) {
+    return absl::InternalError(
+        absl::StrCat("Unable to create channel to ", subprocess_info.address));
+  }
+  subprocess_info.profiler_stub =
+      tensorflow::grpc::ProfilerService::NewStub(channel);
+  {
+    absl::MutexLock l(&registry().mu);
+    if (!registry().subprocesses.insert(subprocess_info).second) {
+      return absl::AlreadyExistsError(
+          absl::StrCat(subprocess_info.DebugString(), " already registered"));
+    }
+  }
+  return absl::OkStatus();
+}
+
+absl::Status RegisterSubprocess(pid_t pid, int port) {
+  return RegisterSubprocess({pid, absl::StrCat("localhost:", port)});
+}
+
+absl::Status RegisterSubprocess(pid_t pid,
+                                absl::string_view unix_domain_socket) {
+  return RegisterSubprocess({pid, absl::StrCat("unix:", unix_domain_socket)});
+}
+
+absl::Status UnregisterSubprocess(pid_t pid) {
+  absl::MutexLock l(&registry().mu);
+  if (registry().subprocesses.erase({pid, ""}) == 0) {
+    LOG(WARNING) << "Subprocess " << pid << " not found";
+    return absl::NotFoundError(absl::StrCat(pid, " not found"));
+  }
+  return absl::OkStatus();
+}
+
+std::vector<SubprocessInfo> GetRegisteredSubprocesses() {
+  absl::MutexLock l(&registry().mu);
+  std::vector<SubprocessInfo> subprocesses;
+  subprocesses.reserve(registry().subprocesses.size());
+  for (const SubprocessInfo& subprocess_info : registry().subprocesses) {
+    subprocesses.push_back(subprocess_info);
+  }
+  return subprocesses;
+}
+
+}  // namespace cpu
+}  // namespace profiler
+}  // namespace xla

third_party/xla/xla/backends/profiler/cpu/subprocess_registry.h
@@ -1,0 +1,78 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef XLA_BACKENDS_PROFILER_CPU_SUBPROCESS_REGISTRY_H_
+#define XLA_BACKENDS_PROFILER_CPU_SUBPROCESS_REGISTRY_H_
+
+#include <sys/types.h>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "absl/status/status.h"
+#include "absl/strings/string_view.h"
+#include "tsl/profiler/protobuf/profiler_service.grpc.pb.h"
+
+namespace xla {
+namespace profiler {
+namespace cpu {
+
+// Information about a registered subprocess.
+struct SubprocessInfo {
+  pid_t pid;
+  std::string address;
+  std::shared_ptr<tensorflow::grpc::ProfilerService::Stub> profiler_stub;
+
+  template <typename H>
+  friend H AbslHashValue(H h, const SubprocessInfo& subprocess) {
+    return H::combine(std::move(h), subprocess.pid);
+  }
+
+  bool operator==(const SubprocessInfo& other) const {
+    return pid == other.pid;
+  }
+
+  bool operator!=(const SubprocessInfo& other) const {
+    return !(*this == other);
+  }
+
+  std::string DebugString() const;
+};
+
+// Registers a subprocess that has a running HTTP server listening on the given
+// port or Unix domain socket, so that it can be profiled using the
+// subprocess profiler.
+// It is expected to call `RegisterSubprocess` after starting the subprocess.
+// This method will create a stub to the ProfilerService in the subprocess, and
+// may block for a while until the stub is ready or connection times out.
+// RETURNS: an error if the subprocess is already registered or if the
+// subprocess stub cannot be created.
+absl::Status RegisterSubprocess(pid_t pid, int port);
+absl::Status RegisterSubprocess(pid_t pid,
+                                absl::string_view unix_domain_socket);
+
+// Unregisters a subprocess by just erasing it from the registry. If there are
+// in-flight profiling sessions, they will NOT be cancelled.
+absl::Status UnregisterSubprocess(pid_t pid);
+
+// Returns all currently registered subprocesses.
+std::vector<SubprocessInfo> GetRegisteredSubprocesses();
+
+}  // namespace cpu
+}  // namespace profiler
+}  // namespace xla
+
+#endif  // XLA_BACKENDS_PROFILER_CPU_SUBPROCESS_REGISTRY_H_

third_party/xla/xla/backends/profiler/cpu/subprocess_registry_test.cc
@@ -1,0 +1,52 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "xla/backends/profiler/cpu/subprocess_registry.h"
+
+#include "xla/tsl/platform/env.h"
+#include "xla/tsl/platform/test.h"
+
+namespace xla {
+namespace profiler {
+namespace cpu {
+namespace {
+
+using ::testing::Not;
+using ::testing::status::IsOk;
+
+TEST(SubprocessRegistryTest, FailsToRegisterCurrentProcessAsSubprocess) {
+  EXPECT_THAT(RegisterSubprocess(tsl::Env::Default()->GetProcessId(), 1234),
+              Not(IsOk()));
+}
+
+TEST(SubprocessRegistryTest, RegistersSubprocessWithPort) {
+  EXPECT_THAT(RegisterSubprocess(1234, tsl::testing::PickUnusedPortOrDie()),
+              IsOk());
+}
+
+TEST(SubprocessRegistryTest, RegistersSubprocessWithUnixDomainSocket) {
+  EXPECT_THAT(RegisterSubprocess(12345, "/tmp/valid_socket"), IsOk());
+}
+
+TEST(SubprocessRegistryTest, FailsToRegisterSubprocessTwice) {
+  int port = tsl::testing::PickUnusedPortOrDie();
+  EXPECT_THAT(RegisterSubprocess(123456, port), IsOk());
+  EXPECT_THAT(RegisterSubprocess(123456, port), Not(IsOk()));
+}
+
+}  // namespace
+}  // namespace cpu
+}  // namespace profiler
+}  // namespace xla

CodeLinaro-mirror/lc_external_github.com_tensorflow_tensorflow

third_party/xla/xla/backends/profiler/subprocess/subprocess_registry.cc
@@ -1,0 +1,119 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "xla/backends/profiler/subprocess/subprocess_registry.h"
+
+#include <cstdint>
+#include <limits>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "absl/base/no_destructor.h"
+#include "absl/base/thread_annotations.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/log/log.h"
+#include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/string_view.h"
+#include "absl/synchronization/mutex.h"
+#include "grpcpp/security/credentials.h"
+#include "grpcpp/support/channel_arguments.h"
+#include "xla/tsl/platform/env.h"
+#include "tsl/profiler/protobuf/profiler_service.grpc.pb.h"
+
+namespace xla {
+namespace profiler {
+namespace subprocess {
+namespace {
+
+// Registry of subprocesses.
+struct Registry {
+  absl::Mutex mu;
+  absl::flat_hash_set<SubprocessInfo> subprocesses ABSL_GUARDED_BY(mu);
+};
+
+// Global registry of subprocesses.
+Registry& registry() {
+  static absl::NoDestructor<Registry> registry;
+  return *registry;
+}
+
+absl::Status RegisterSubprocess(SubprocessInfo&& subprocess_info) {
+  // This check ensures that there can't be a loop in the subprocess profiler
+  // graph.
+  if (subprocess_info.pid == tsl::Env::Default()->GetProcessId()) {
+    return absl::InvalidArgumentError(
+        "Cannot register subprocess with the same pid as current process.");
+  }
+  ::grpc::ChannelArguments channel_args;
+  channel_args.SetMaxReceiveMessageSize(std::numeric_limits<int32_t>::max());
+  auto channel = ::grpc::CreateCustomChannel(
+      subprocess_info.address, ::grpc::InsecureChannelCredentials(),
+      channel_args);
+  if (!channel) {
+    return absl::InternalError(
+        absl::StrCat("Unable to create channel to ", subprocess_info.address));
+  }
+  subprocess_info.profiler_stub =
+      tensorflow::grpc::ProfilerService::NewStub(channel);
+  {
+    absl::MutexLock l(&registry().mu);
+    if (!registry().subprocesses.insert(subprocess_info).second) {
+      return absl::AlreadyExistsError(
+          absl::StrCat(subprocess_info.DebugString(), " already registered"));
+    }
+  }
+  return absl::OkStatus();
+}
+
+}  // namespace
+
+std::string SubprocessInfo::DebugString() const {
+  return absl::StrCat("SubprocessInfo(pid: ", pid, ", address: ", address, ")");
+}
+
+absl::Status RegisterSubprocess(pid_t pid, int port) {
+  return RegisterSubprocess({pid, absl::StrCat("localhost:", port)});
+}
+
+absl::Status RegisterSubprocess(pid_t pid,
+                                absl::string_view unix_domain_socket) {
+  return RegisterSubprocess({pid, absl::StrCat("unix:", unix_domain_socket)});
+}
+
+absl::Status UnregisterSubprocess(pid_t pid) {
+  absl::MutexLock l(&registry().mu);
+  if (registry().subprocesses.erase({pid, ""}) == 0) {
+    LOG(WARNING) << "Subprocess " << pid << " not found";
+    return absl::NotFoundError(absl::StrCat(pid, " not found"));
+  }
+  return absl::OkStatus();
+}
+
+std::vector<SubprocessInfo> GetRegisteredSubprocesses() {
+  absl::MutexLock l(&registry().mu);
+  std::vector<SubprocessInfo> subprocesses;
+  subprocesses.reserve(registry().subprocesses.size());
+  for (const SubprocessInfo& subprocess_info : registry().subprocesses) {
+    subprocesses.push_back(subprocess_info);
+  }
+  return subprocesses;
+}
+
+}  // namespace subprocess
+}  // namespace profiler
+}  // namespace xla

third_party/xla/xla/backends/profiler/subprocess/subprocess_registry.h
@@ -1,0 +1,78 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef XLA_BACKENDS_PROFILER_SUBPROCESS_SUBPROCESS_REGISTRY_H_
+#define XLA_BACKENDS_PROFILER_SUBPROCESS_SUBPROCESS_REGISTRY_H_
+
+#include <sys/types.h>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "absl/status/status.h"
+#include "absl/strings/string_view.h"
+#include "tsl/profiler/protobuf/profiler_service.grpc.pb.h"
+
+namespace xla {
+namespace profiler {
+namespace subprocess {
+
+// Information about a registered subprocess.
+struct SubprocessInfo {
+  pid_t pid;
+  std::string address;
+  std::shared_ptr<tensorflow::grpc::ProfilerService::Stub> profiler_stub;
+
+  template <typename H>
+  friend H AbslHashValue(H h, const SubprocessInfo& subprocess) {
+    return H::combine(std::move(h), subprocess.pid);
+  }
+
+  bool operator==(const SubprocessInfo& other) const {
+    return pid == other.pid;
+  }
+
+  bool operator!=(const SubprocessInfo& other) const {
+    return !(*this == other);
+  }
+
+  std::string DebugString() const;
+};
+
+// Registers a subprocess that has a running HTTP server listening on the given
+// port or Unix domain socket, so that it can be profiled using the
+// subprocess profiler.
+// It is expected to call `RegisterSubprocess` after starting the subprocess.
+// This method will create a stub to the ProfilerService in the subprocess, and
+// may block for a while until the stub is ready or connection times out.
+// RETURNS: an error if the subprocess is already registered or if the
+// subprocess stub cannot be created.
+absl::Status RegisterSubprocess(pid_t pid, int port);
+absl::Status RegisterSubprocess(pid_t pid,
+                                absl::string_view unix_domain_socket);
+
+// Unregisters a subprocess by just erasing it from the registry. If there are
+// in-flight profiling sessions, they will NOT be cancelled.
+absl::Status UnregisterSubprocess(pid_t pid);
+
+// Returns all currently registered subprocesses.
+std::vector<SubprocessInfo> GetRegisteredSubprocesses();
+
+}  // namespace subprocess
+}  // namespace profiler
+}  // namespace xla
+
+#endif  // XLA_BACKENDS_PROFILER_SUBPROCESS_SUBPROCESS_REGISTRY_H_

third_party/xla/xla/backends/profiler/subprocess/subprocess_registry_test.cc
@@ -1,0 +1,53 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "xla/backends/profiler/subprocess/subprocess_registry.h"
+
+#include "absl/status/status_matchers.h"
+#include "xla/tsl/platform/env.h"
+#include "xla/tsl/platform/test.h"
+
+namespace xla {
+namespace profiler {
+namespace subprocess {
+namespace {
+
+using absl_testing::IsOk;
+using ::testing::Not;
+
+TEST(SubprocessRegistryTest, FailsToRegisterCurrentProcessAsSubprocess) {
+  EXPECT_THAT(RegisterSubprocess(tsl::Env::Default()->GetProcessId(), 1234),
+              Not(IsOk()));
+}
+
+TEST(SubprocessRegistryTest, RegistersSubprocessWithPort) {
+  EXPECT_THAT(RegisterSubprocess(1234, tsl::testing::PickUnusedPortOrDie()),
+              IsOk());
+}
+
+TEST(SubprocessRegistryTest, RegistersSubprocessWithUnixDomainSocket) {
+  EXPECT_THAT(RegisterSubprocess(12345, "/tmp/valid_socket"), IsOk());
+}
+
+TEST(SubprocessRegistryTest, FailsToRegisterSubprocessTwice) {
+  int port = tsl::testing::PickUnusedPortOrDie();
+  EXPECT_THAT(RegisterSubprocess(123456, port), IsOk());
+  EXPECT_THAT(RegisterSubprocess(123456, port), Not(IsOk()));
+}
+
+}  // namespace
+}  // namespace subprocess
+}  // namespace profiler
+}  // namespace xla