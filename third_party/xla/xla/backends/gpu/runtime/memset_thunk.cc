9d0fe90fbb7b7c8d5e719f689dea8ae715691f71
A. Unique TensorFlower <gardener@tensorflow.org>
2025-09-09 14:38:06 -0700

    [XLA:GPU] Add proto serialization for MemzeroThunk
    PiperOrigin-RevId: 805069280

--- third_party/xla/xla/backends/gpu/runtime/memset_thunk.cc
+++ third_party/xla/xla/backends/gpu/runtime/memset_thunk.cc
@@ -17,6 +17,7 @@ limitations under the License.
 
 #include "absl/status/status.h"
 #include "xla/stream_executor/stream_executor.h"
+#include "xla/tsl/platform/statusor.h"
 
 namespace xla {
 namespace gpu {
@@ -25,6 +26,25 @@ absl::Status MemzeroThunk::ExecuteOnStream(const ExecuteParams& params) {
   se::DeviceMemoryBase dest_data =
       params.buffer_allocations->GetDeviceAddress(dest_);
   return params.stream->MemZero(&dest_data, dest_data.size());
+}
+
+absl::StatusOr<std::unique_ptr<MemzeroThunk>> MemzeroThunk::FromProto(
+    ThunkInfo thunk_info, const MemzeroThunkProto& thunk_proto,
+    absl::Span<const BufferAllocation> buffer_allocations) {
+  TF_ASSIGN_OR_RETURN(BufferAllocation::Slice dest,
+                      BufferAllocation::Slice::FromProto(
+                          thunk_proto.dest_buffer(), buffer_allocations));
+  return std::make_unique<MemzeroThunk>(std::move(thunk_info), dest);
+}
+
+absl::StatusOr<ThunkProto> MemzeroThunk::ToProto() const {
+  ThunkProto proto;
+  *proto.mutable_thunk_info() = thunk_info().ToProto();
+
+  auto* memzero_thunk_proto = proto.mutable_memzero_thunk();
+  TF_ASSIGN_OR_RETURN(*memzero_thunk_proto->mutable_dest_buffer(),
+                      dest_.ToProto());
+  return proto;
 }
 
 absl::Status Memset32BitValueThunk::ExecuteOnStream(
