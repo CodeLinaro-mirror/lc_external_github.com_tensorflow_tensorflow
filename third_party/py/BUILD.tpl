hexagon-geo-surv/tensorflow

Update `rules_python` to v1.6 in XLA and TensorFlow projects.
`com_google_protobuf` and `rules_cc` initialization was moved to `python_init_rules` macros.

The JAX repository update will be done [in the next step](https://github.com/jax-ml/jax/pull/31709)

PiperOrigin-RevId: 803189916

third_party/py/BUILD.tpl
@@ -5,17 +5,16 @@ package(default_visibility = ["//visibility:public"])
 # Point both runtimes to the same python binary to ensure we always
 # use the python binary specified by ./configure.py script.
 load("@bazel_tools//tools/python:toolchain.bzl", "py_runtime_pair")
-load("@python//:defs.bzl", "interpreter")
 
 py_runtime(
     name = "py2_runtime",
-    interpreter_path = interpreter,
+    interpreter_path = "%{PYTHON_INTERPRETER}",
     python_version = "PY2",
 )
 
 py_runtime(
     name = "py3_runtime",
-    interpreter_path = interpreter,
+    interpreter_path = "%{PYTHON_INTERPRETER}",
     python_version = "PY3",
 )
 
@@ -34,7 +33,7 @@ toolchain(
 )
 
 alias(name = "python_headers",
-      actual = "@python//:python_headers")
+      actual = "@rules_python//python/cc:current_py_cc_headers")
 
 # This alias is exists for the use of targets in the @llvm-project dependency,
 # which expect a python_headers target called @python_runtime//:headers. We use

CodeLinaro-mirror/lc_external_github.com_tensorflow_tensorflow

third_party/py/BUILD.tpl
@@ -5,17 +5,16 @@ package(default_visibility = ["//visibility:public"])
 # Point both runtimes to the same python binary to ensure we always
 # use the python binary specified by ./configure.py script.
 load("@bazel_tools//tools/python:toolchain.bzl", "py_runtime_pair")
-load("@python//:defs.bzl", "interpreter")
 
 py_runtime(
     name = "py2_runtime",
-    interpreter_path = interpreter,
+    interpreter = "%{PYTHON_INTERPRETER}",
     python_version = "PY2",
 )
 
 py_runtime(
     name = "py3_runtime",
-    interpreter_path = interpreter,
+    interpreter = "%{PYTHON_INTERPRETER}",
     python_version = "PY3",
 )
 
@@ -34,7 +33,7 @@ toolchain(
 )
 
 alias(name = "python_headers",
-      actual = "@python//:python_headers")
+      actual = "@rules_python//python/cc:current_py_cc_headers")
 
 # This alias is exists for the use of targets in the @llvm-project dependency,
 # which expect a python_headers target called @python_runtime//:headers. We use