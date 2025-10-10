CodeLinaro-mirror/yocto-mirrors_github_tensorflow_tensorflow

[XLA:GPU]: Add support for loading HLO directly from profiler to graphviz
No change to the OSS version for this tool.

PiperOrigin-RevId: 816662783

third_party/xla/xla/tools/interactive_graphviz.cc
@@ -30,6 +30,7 @@ limitations under the License.
 #include <cstdint>
 #include <iostream>
 #include <memory>
+#include <optional>
 #include <ostream>
 #include <string>
 #include <utility>
@@ -38,6 +39,7 @@ limitations under the License.
 #include "absl/algorithm/container.h"
 #include "absl/container/flat_hash_set.h"
 #include "absl/log/log.h"
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/match.h"
 #include "absl/strings/numbers.h"
@@ -64,11 +66,15 @@ limitations under the License.
 #include "xla/tsl/platform/env.h"
 #include "xla/tsl/platform/errors.h"
 #include "xla/tsl/platform/status.h"
+#include "xla/tsl/platform/statusor.h"
 #include "xla/tsl/platform/subprocess.h"
+#include "xla/tsl/platform/xprof_loader.h"
 #include "xla/tsl/protobuf/error_codes.pb.h"
 #include "xla/tsl/util/command_line_flags.h"
+#include "xla/util.h"
 #include "tsl/platform/init_main.h"
 #include "tsl/platform/path.h"
+#include "tsl/platform/platform.h"
 #if defined(PLATFORM_GOOGLE)
 #include "util/readline/readline.h"
 #endif
@@ -99,8 +105,12 @@ struct Options {
   std::string hlo_proto;
   std::string hlo_module_proto;
   std::string hlo_text;
+  std::string xprof_session_id;
+  int64_t xprof_hlo_program_id{-1};  // -1 means use the biggest HLO module.
   std::string platform;
   std::string browser;
+  // Help flag to print usage.
+  bool help{false};
 };
 
 const char* const kUsage = R"(
@@ -692,6 +702,21 @@ void InteractiveDumpGraphs(const Options& opts, const HloModule& module) {
   }
 }
 
+absl::StatusOr<std::unique_ptr<HloModule>> ReadModuleFromXprof(
+    std::string xprof_session_id, int64_t xprof_hlo_program_id,
+    const DebugOptions& debug_options) {
+  std::cout << "Reading module from Xprof session: " << xprof_session_id
+            << std::endl;
+  XLA_SCOPED_LOGGING_TIMER_LEVEL("ReadModuleFromXprof", 0);
+  const std::optional<uint64_t> xprof_hlo_program_id_optional =
+      xprof_hlo_program_id >= 0 ? std::make_optional(xprof_hlo_program_id)
+                                : std::nullopt;
+  TF_ASSIGN_OR_RETURN(
+      auto hlo_module_proto,
+      LoadHloModuleFromXprof(xprof_session_id, xprof_hlo_program_id_optional));
+  return CreateModuleFromProto(hlo_module_proto, debug_options);
+}
+
 void CheckFlags(const Options& opts) {
   int nonempty_flags_amount = 0;
   if (!opts.hlo_proto.empty()) {
@@ -706,11 +731,15 @@ void CheckFlags(const Options& opts) {
   if (!opts.hlo_module_proto.empty()) {
     ++nonempty_flags_amount;
   }
+  if (!opts.xprof_session_id.empty()) {
+    ++nonempty_flags_amount;
+  }
   if (nonempty_flags_amount == 1) {
     return;
   }
   LOG(FATAL) << "Can only specify one and only one of '--hlo_proto', "
-                "'--hlo_snapshot', '--hlo_text', '--hlo_module_proto' flags.";
+                "'--hlo_snapshot', '--hlo_text', '--hlo_module_proto' or "
+                "'--xprof_session_id' flags.";
 }
 
 void RealMain(const Options& opts) {
@@ -747,6 +776,11 @@ void RealMain(const Options& opts) {
     module = ReadModuleFromModuleBinaryProtofile(
                  opts.hlo_module_proto, xla::GetDebugOptionsFromFlags())
                  .value();
+  } else if (!opts.xprof_session_id.empty()) {
+    module =
+        ReadModuleFromXprof(opts.xprof_session_id, opts.xprof_hlo_program_id,
+                            xla::GetDebugOptionsFromFlags())
+            .value();
   }
 
   // If a platform was specified, compile the module for that platform.
@@ -771,15 +805,8 @@ void RealMain(const Options& opts) {
   }
 }
 
-}  // namespace
-}  // namespace tools
-}  // namespace xla
-
-int main(int argc, char** argv) {
-  xla::tools::Options opts;
-  opts.browser = "/usr/bin/sensible-browser";
-  bool need_help = false;
-  const std::vector<tsl::Flag> flag_list = {
+std::vector<tsl::Flag> GetFlagList(xla::tools::Options& opts) {
+  std::vector<tsl::Flag> flag_list = {
       tsl::Flag("hlo_snapshot", &opts.hlo_snapshot,
                 "HloSnapshot proto to interactively dump to graphviz"),
       tsl::Flag("hlo_proto", &opts.hlo_proto,
@@ -792,12 +819,36 @@ int main(int argc, char** argv) {
                 "Platform to compile for: CPU, CUDA, etc"),
       tsl::Flag("browser", &opts.browser,
                 "Path to web browser used to display produced graphs."),
-      tsl::Flag("help", &need_help, "Prints this help message"),
+      tsl::Flag("help", &opts.help, "Prints this help message"),
   };
+  if constexpr (!tsl::kIsOpenSource) {
+    flag_list.push_back(
+        tsl::Flag("xprof_session_id", &opts.xprof_session_id,
+                  "XProf session ID to pull HLO proto from and interactively "
+                  "dump to graphviz. Not valid for OSS."));
+    flag_list.push_back(tsl::Flag(
+        "xprof_hlo_program_id", &opts.xprof_hlo_program_id,
+        "The Program ID of the HLO module to pull from the XProf session. "
+        "Modules in Xprof are identified by <Name>(<Id>). For example, "
+        "'jit_prefill(1175)'. Here 1175 is the program id. "
+        "If not specified, the tool will try to find the largest program by "
+        "HLO bytes size."));
+  }
+  return flag_list;
+}
+
+}  // namespace
+}  // namespace tools
+}  // namespace xla
+
+int main(int argc, char** argv) {
+  xla::tools::Options opts;
+  opts.browser = "/usr/bin/sensible-browser";
+  const auto flag_list = xla::tools::GetFlagList(opts);
   std::string usage = tsl::Flags::Usage(argv[0], flag_list);
   bool parse_ok = tsl::Flags::Parse(&argc, argv, flag_list);
   tsl::port::InitMain(argv[0], &argc, &argv);
-  if (argc != 1 || !parse_ok || need_help) {
+  if (argc != 1 || !parse_ok || opts.help) {
     LOG(QFATAL) << usage;
   }
   xla::tools::RealMain(opts);

third_party/xla/xla/tsl/platform/default/xprof_loader.cc
@@ -1,0 +1,33 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "xla/tsl/platform/xprof_loader.h"
+
+#include <cstdint>
+#include <optional>
+#include <string>
+
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "xla/service/hlo.pb.h"
+
+namespace xla {
+
+absl::StatusOr<HloModuleProto> LoadHloModuleFromXprof(
+    std::string xprof_session_id,
+    std::optional<uint64_t> xprof_hlo_program_id) {
+  return absl::UnimplementedError("Not implemented for non-google platforms.");
+}
+}  // namespace xla

third_party/xla/xla/tsl/platform/xprof_loader.h
@@ -1,0 +1,31 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef XLA_TSL_PLATFORM_XPROF_LOADER_H_
+#define XLA_TSL_PLATFORM_XPROF_LOADER_H_
+
+#include <cstdint>
+#include <optional>
+#include <string>
+
+#include "absl/status/statusor.h"
+#include "xla/service/hlo.pb.h"
+
+namespace xla {
+absl::StatusOr<HloModuleProto> LoadHloModuleFromXprof(
+    std::string xprof_session_id, std::optional<uint64_t> xprof_hlo_program_id);
+}  // namespace xla
+
+#endif  // XLA_TSL_PLATFORM_XPROF_LOADER_H_

CodeLinaro-mirror/lc_external_github.com_tensorflow_tensorflow

third_party/xla/xla/tools/interactive_graphviz.cc
@@ -30,6 +30,7 @@ limitations under the License.
 #include <cstdint>
 #include <iostream>
 #include <memory>
+#include <optional>
 #include <ostream>
 #include <string>
 #include <utility>
@@ -38,6 +39,7 @@ limitations under the License.
 #include "absl/algorithm/container.h"
 #include "absl/container/flat_hash_set.h"
 #include "absl/log/log.h"
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/match.h"
 #include "absl/strings/numbers.h"
@@ -61,14 +63,18 @@ limitations under the License.
 #include "xla/stream_executor/platform.h"
 #include "xla/stream_executor/stream_executor.h"
 #include "xla/tools/hlo_extractor.h"
+#include "xla/tools/platform/xprof_loader.h"
 #include "xla/tsl/platform/env.h"
 #include "xla/tsl/platform/errors.h"
 #include "xla/tsl/platform/status.h"
+#include "xla/tsl/platform/statusor.h"
 #include "xla/tsl/platform/subprocess.h"
 #include "xla/tsl/protobuf/error_codes.pb.h"
 #include "xla/tsl/util/command_line_flags.h"
+#include "xla/util.h"
 #include "tsl/platform/init_main.h"
 #include "tsl/platform/path.h"
+#include "tsl/platform/platform.h"
 #if defined(PLATFORM_GOOGLE)
 #include "util/readline/readline.h"
 #endif
@@ -99,8 +105,12 @@ struct Options {
   std::string hlo_proto;
   std::string hlo_module_proto;
   std::string hlo_text;
+  std::string xprof_session_id;
+  int64_t xprof_hlo_program_id{-1};  // -1 means use the biggest HLO module.
   std::string platform;
   std::string browser;
+  // Help flag to print usage.
+  bool help{false};
 };
 
 const char* const kUsage = R"(
@@ -692,6 +702,21 @@ void InteractiveDumpGraphs(const Options& opts, const HloModule& module) {
   }
 }
 
+absl::StatusOr<std::unique_ptr<HloModule>> ReadModuleFromXprof(
+    std::string xprof_session_id, int64_t xprof_hlo_program_id,
+    const DebugOptions& debug_options) {
+  std::cout << "Reading module from Xprof session: " << xprof_session_id
+            << std::endl;
+  XLA_SCOPED_LOGGING_TIMER_LEVEL("ReadModuleFromXprof", 0);
+  const std::optional<uint64_t> xprof_hlo_program_id_optional =
+      xprof_hlo_program_id >= 0 ? std::make_optional(xprof_hlo_program_id)
+                                : std::nullopt;
+  TF_ASSIGN_OR_RETURN(
+      auto hlo_module_proto,
+      LoadHloModuleFromXprof(xprof_session_id, xprof_hlo_program_id_optional));
+  return CreateModuleFromProto(hlo_module_proto, debug_options);
+}
+
 void CheckFlags(const Options& opts) {
   int nonempty_flags_amount = 0;
   if (!opts.hlo_proto.empty()) {
@@ -706,11 +731,15 @@ void CheckFlags(const Options& opts) {
   if (!opts.hlo_module_proto.empty()) {
     ++nonempty_flags_amount;
   }
+  if (!opts.xprof_session_id.empty()) {
+    ++nonempty_flags_amount;
+  }
   if (nonempty_flags_amount == 1) {
     return;
   }
   LOG(FATAL) << "Can only specify one and only one of '--hlo_proto', "
-                "'--hlo_snapshot', '--hlo_text', '--hlo_module_proto' flags.";
+                "'--hlo_snapshot', '--hlo_text', '--hlo_module_proto' or "
+                "'--xprof_session_id' flags.";
 }
 
 void RealMain(const Options& opts) {
@@ -747,6 +776,11 @@ void RealMain(const Options& opts) {
     module = ReadModuleFromModuleBinaryProtofile(
                  opts.hlo_module_proto, xla::GetDebugOptionsFromFlags())
                  .value();
+  } else if (!opts.xprof_session_id.empty()) {
+    module =
+        ReadModuleFromXprof(opts.xprof_session_id, opts.xprof_hlo_program_id,
+                            xla::GetDebugOptionsFromFlags())
+            .value();
   }
 
   // If a platform was specified, compile the module for that platform.
@@ -771,15 +805,8 @@ void RealMain(const Options& opts) {
   }
 }
 
-}  // namespace
-}  // namespace tools
-}  // namespace xla
-
-int main(int argc, char** argv) {
-  xla::tools::Options opts;
-  opts.browser = "/usr/bin/sensible-browser";
-  bool need_help = false;
-  const std::vector<tsl::Flag> flag_list = {
+std::vector<tsl::Flag> GetFlagList(xla::tools::Options& opts) {
+  std::vector<tsl::Flag> flag_list = {
       tsl::Flag("hlo_snapshot", &opts.hlo_snapshot,
                 "HloSnapshot proto to interactively dump to graphviz"),
       tsl::Flag("hlo_proto", &opts.hlo_proto,
@@ -792,12 +819,36 @@ int main(int argc, char** argv) {
                 "Platform to compile for: CPU, CUDA, etc"),
       tsl::Flag("browser", &opts.browser,
                 "Path to web browser used to display produced graphs."),
-      tsl::Flag("help", &need_help, "Prints this help message"),
+      tsl::Flag("help", &opts.help, "Prints this help message"),
   };
+  if constexpr (!tsl::kIsOpenSource) {
+    flag_list.push_back(
+        tsl::Flag("xprof_session_id", &opts.xprof_session_id,
+                  "XProf session ID to pull HLO proto from and interactively "
+                  "dump to graphviz. Not valid for OSS."));
+    flag_list.push_back(tsl::Flag(
+        "xprof_hlo_program_id", &opts.xprof_hlo_program_id,
+        "The Program ID of the HLO module to pull from the XProf session. "
+        "Modules in Xprof are identified by <Name>(<Id>). For example, "
+        "'jit_prefill(1175)'. Here 1175 is the program id. "
+        "If not specified, the tool will try to find the largest program by "
+        "HLO bytes size."));
+  }
+  return flag_list;
+}
+
+}  // namespace
+}  // namespace tools
+}  // namespace xla
+
+int main(int argc, char** argv) {
+  xla::tools::Options opts;
+  opts.browser = "/usr/bin/sensible-browser";
+  const auto flag_list = xla::tools::GetFlagList(opts);
   std::string usage = tsl::Flags::Usage(argv[0], flag_list);
   bool parse_ok = tsl::Flags::Parse(&argc, argv, flag_list);
   tsl::port::InitMain(argv[0], &argc, &argv);
-  if (argc != 1 || !parse_ok || need_help) {
+  if (argc != 1 || !parse_ok || opts.help) {
     LOG(QFATAL) << usage;
   }
   xla::tools::RealMain(opts);

third_party/xla/xla/tools/platform/default/xprof_loader.cc
@@ -1,0 +1,33 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "xla/tools/platform/xprof_loader.h"
+
+#include <cstdint>
+#include <optional>
+#include <string>
+
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "xla/service/hlo.pb.h"
+
+namespace xla {
+
+absl::StatusOr<HloModuleProto> LoadHloModuleFromXprof(
+    std::string xprof_session_id,
+    std::optional<uint64_t> xprof_hlo_program_id) {
+  return absl::UnimplementedError("Not implemented for non-google platforms.");
+}
+}  // namespace xla

third_party/xla/xla/tools/platform/xprof_loader.h
@@ -1,0 +1,31 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef XLA_TOOLS_PLATFORM_XPROF_LOADER_H_
+#define XLA_TOOLS_PLATFORM_XPROF_LOADER_H_
+
+#include <cstdint>
+#include <optional>
+#include <string>
+
+#include "absl/status/statusor.h"
+#include "xla/service/hlo.pb.h"
+
+namespace xla {
+absl::StatusOr<HloModuleProto> LoadHloModuleFromXprof(
+    std::string xprof_session_id, std::optional<uint64_t> xprof_hlo_program_id);
+}  // namespace xla
+
+#endif  // XLA_TOOLS_PLATFORM_XPROF_LOADER_H_