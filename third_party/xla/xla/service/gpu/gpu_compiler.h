afbbdbfa1e85562aa802eb075d5d8be169a923d5
Eusebio Durán Montaña <eusebiodm@google.com>
2025-11-06 02:04:25 -0800

    Move `GpuThunkGpuAotCompilationResult` to a new file, and rename it.
    Moving it out of the `gpu_compiler.cc` since that class is already huge.
    
    Renaming to legacy since it will be replaced the new result form the runtime split project, and removing the "thunk" part since this has nothing to do with thunks (unlike the new upcoming result).
    
    Unfortunately we had to create a `LoadExecutableFromAotResult` function in compiler.h to avoid a circular dependency in between `LegacyGpuAotCompilationResult` and `GpuCompiler`.
    
    Also fixing some includes and braces along the way.
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/33106 from dimvar:cudnn-cuda-graph b2f357e89343eaf5325df9b88b68acd04f6fdf87
    PiperOrigin-RevId: 828854427

--- third_party/xla/xla/service/gpu/gpu_compiler.h
+++ third_party/xla/xla/service/gpu/gpu_compiler.h
@@ -139,6 +139,10 @@ class GpuCompiler : public LLVMCompiler {
       AlgebraicSimplifierMode mode, const DebugOptions& debug_options,
       bool is_rocm);
 
+  absl::StatusOr<std::unique_ptr<Executable>> LoadExecutableFromAotResult(
+      std::unique_ptr<AotCompilationResult> aot_result,
+      const se::StreamExecutor& stream_exec) override;
+
  protected:
   struct BackendCompileResult {
     std::string asm_text;
