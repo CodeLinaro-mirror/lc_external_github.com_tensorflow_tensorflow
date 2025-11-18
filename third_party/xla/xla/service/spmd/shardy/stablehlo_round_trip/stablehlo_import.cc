0cd89c127d638b9ab0de6b283f6e14f7b02d2145
Zixuan Jiang <zixuanjiang@google.com>
2025-11-17 16:11:40 -0800

    Disable `StablehloCanonicalizeFromHloImportPass` in the Shardy `StablehloImportPipeline`.
    `stablehlo-import` is not in production and only used in auto-sharding and deprecated tests. This cl intends to resolve b/459908800.
    
    PiperOrigin-RevId: 833537227

--- third_party/xla/xla/service/spmd/shardy/stablehlo_round_trip/stablehlo_import.cc
+++ third_party/xla/xla/service/spmd/shardy/stablehlo_round_trip/stablehlo_import.cc
@@ -752,7 +752,8 @@ void addStablehloImportPipeline(mlir::OpPassManager& pm,
                                 ArrayRef<bool> allowPropagationToArgs,
                                 ArrayRef<bool> allowPropagationToResults,
                                 bool importFuncCalls) {
-  addCommonPreImportPasses(pm);
+  addCommonPreImportPasses(pm, /*enableConstantImport=*/true,
+                           /*enableStablehloCanonicalizeFromHloImport=*/false);
   pm.addPass(createImportShardingsPass(allowPropagationToArgs,
                                        allowPropagationToResults));
   pm.addPass(createStablehloRoundTripShardMapImportPass());
