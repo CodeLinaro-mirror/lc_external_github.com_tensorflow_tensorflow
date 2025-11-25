e249c83e2aa79f450126feb179f86a3119b68882
Marcin Radomski <dextero@google.com>
2025-11-25 03:34:32 -0800

    [XLA] Require trailing semicolon after TF_ASSERT_OK_AND_ASSIGN
    To get the behavior in line with internal ASSERT_OK_AND_ASSIGN.
    Unlike the non-TF_ counterpart, the macro expands to code that already
    includes the trailing semicolon so it happens to work.
    
    Attempts to replace it with an internal variant as part of b/444419873
    make it not work anymore, until the missing semicolons are added.
    
    PiperOrigin-RevId: 836595367

--- third_party/xla/xla/tsl/platform/statusor.h
+++ third_party/xla/xla/tsl/platform/statusor.h
@@ -90,10 +90,9 @@ using StatusOr ABSL_DEPRECATE_AND_INLINE() = absl::StatusOr<T>;
 
 }  // namespace tsl
 
-#define TF_ASSERT_OK_AND_ASSIGN(lhs, rexpr)                             \
-  TF_ASSERT_OK_AND_ASSIGN_IMPL(                                         \
-      TF_STATUS_MACROS_CONCAT_NAME(_status_or_value, __COUNTER__), lhs, \
-      rexpr);
+#define TF_ASSERT_OK_AND_ASSIGN(lhs, rexpr) \
+  TF_ASSERT_OK_AND_ASSIGN_IMPL(             \
+      TF_STATUS_MACROS_CONCAT_NAME(_status_or_value, __COUNTER__), lhs, rexpr)
 
 #define TF_ASSERT_OK_AND_ASSIGN_IMPL(statusor, lhs, rexpr) \
   auto statusor = (rexpr);                                 \
