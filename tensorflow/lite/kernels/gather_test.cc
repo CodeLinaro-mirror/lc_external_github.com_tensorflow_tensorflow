53e8dde1db017a914e0cc105b2dd56d3b123b6a3
Fengwu Yao <fengwuyao@google.com>
2025-12-01 15:58:00 -0800

    Update to use half data type in test_util
    PiperOrigin-RevId: 838966586

--- tensorflow/lite/kernels/gather_test.cc
+++ tensorflow/lite/kernels/gather_test.cc
@@ -23,6 +23,7 @@ limitations under the License.
 #include "tensorflow/lite/kernels/internal/portable_tensor_utils.h"
 #include "tensorflow/lite/kernels/test_util.h"
 #include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/types/half.h"
 
 namespace tflite {
 namespace {
@@ -252,7 +253,7 @@ TEST_P(GatherOpTest, LastAxis0DIndex) {
 }
 
 using TestTypes = testing::Types<int8_t, uint8_t, int16_t, int32_t, int64_t,
-                                 float, Eigen::half, Eigen::bfloat16>;
+                                 float, half, Eigen::bfloat16>;
 
 template <typename T>
 struct TypedGatherOpTest : public testing::Test {};
