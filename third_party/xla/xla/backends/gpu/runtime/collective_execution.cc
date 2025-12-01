e531f1f8d9c536da6093ccaa9390df2688f4af05
Eugene Zhulenev <ezhulenev@google.com>
2025-11-30 21:27:14 -0800

    [xla:gpu] Add an API to lookup acquired communicator by global id
    PiperOrigin-RevId: 838589943

--- third_party/xla/xla/backends/gpu/runtime/collective_execution.cc
+++ third_party/xla/xla/backends/gpu/runtime/collective_execution.cc
@@ -16,7 +16,6 @@ limitations under the License.
 #include "xla/backends/gpu/runtime/collective_execution.h"
 
 #include <cstdint>
-#include <optional>
 #include <utility>
 #include <vector>
 
@@ -28,7 +27,6 @@ limitations under the License.
 #include "xla/backends/gpu/runtime/collective_cliques.h"
 #include "xla/backends/gpu/runtime/collective_params.h"
 #include "xla/core/collectives/communicator.h"
-#include "xla/core/collectives/rank_id.h"
 #include "xla/debug_options_flags.h"
 #include "xla/runtime/device_id.h"
 #include "xla/service/collective_ops_utils.h"
@@ -134,16 +132,10 @@ absl::StatusOr<GpuCliqueKey> GetGpuCliqueKey(
 
 absl::StatusOr<CommunicatorHandle> GetComm(
     const CollectiveParams& params, const CollectiveCliques& collective_cliques,
-    absl::Span<const ReplicaGroup> replica_groups,
-    CollectiveOpGroupMode group_mode, AsyncStreamKind stream_kind) {
+    const GpuCliqueKey& clique_key) {
   TF_ASSIGN_OR_RETURN(
-      GpuCliqueKey clique_key,
-      GetGpuCliqueKey(params, replica_groups, group_mode, stream_kind));
-
-  std::optional<RankId> rank = clique_key.rank(params.global_device_id);
-  TF_ASSIGN_OR_RETURN(Communicator * comm,
-                      collective_cliques.GetComm(clique_key, *rank));
-
+      Communicator * comm,
+      collective_cliques.GetComm(clique_key, params.global_device_id));
   return CommunicatorHandle(comm, std::move(clique_key));
 }
 
