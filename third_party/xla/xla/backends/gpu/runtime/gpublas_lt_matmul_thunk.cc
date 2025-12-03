04b0179ba132ebff79c945d5ab966d620f857f16
Maxim Ermilov <maximermilov@google.com>
2025-12-03 15:37:13 -0800

    override Thunk::buffer_uses where needed. Final
    Reverts 4eabb7c2130762045b19df02ed88bac5e92c1aac
    
    PiperOrigin-RevId: 839943452

--- third_party/xla/xla/backends/gpu/runtime/gpublas_lt_matmul_thunk.cc
+++ third_party/xla/xla/backends/gpu/runtime/gpublas_lt_matmul_thunk.cc
@@ -26,6 +26,7 @@ limitations under the License.
 #include "absl/status/statusor.h"
 #include "absl/types/span.h"
 #include "xla/backends/gpu/runtime/thunk.h"
+#include "xla/runtime/buffer_use.h"
 #include "xla/service/buffer_assignment.h"
 #include "xla/service/gpu/buffer_allocations.h"
 #include "xla/service/gpu/matmul_utils.h"
@@ -158,6 +159,18 @@ absl::Status CublasLtMatmulThunk::Initialize(const InitializeParams& params) {
     return absl::InternalError("Failed to initialize BLASLT support");
   }
   return absl::OkStatus();
+}
+
+Thunk::BufferUses CublasLtMatmulThunk::buffer_uses() const {
+  Thunk::BufferUses res{
+      BufferUse::Read(a_),       BufferUse::Read(b_),
+      BufferUse::Read(c_),       BufferUse::Write(d_),
+      BufferUse::Read(bias_),    BufferUse::Write(aux_),
+      BufferUse::Read(a_scale_), BufferUse::Read(b_scale_),
+      BufferUse::Read(c_scale_), BufferUse::Read(d_scale_),
+      BufferUse::Write(d_amax_),
+  };
+  return res;
 }
 
 absl::StatusOr<ThunkProto> CublasLtMatmulThunk::ToProto() const {
