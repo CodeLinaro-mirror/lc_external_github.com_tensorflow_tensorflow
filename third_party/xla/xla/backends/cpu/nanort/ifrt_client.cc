e57d611f8817954cf9b1c7d3d91bd9c81e78d102
Ionel Gog <icgog@google.com>
2025-12-13 05:58:54 -0800

    Add platform name to xla::ifrt::Device
    PiperOrigin-RevId: 844067251

--- third_party/xla/xla/backends/cpu/nanort/ifrt_client.cc
+++ third_party/xla/xla/backends/cpu/nanort/ifrt_client.cc
@@ -1260,6 +1260,8 @@ class NanoDevice final : public llvm::RTTIExtends<NanoDevice, ifrt::Device> {
     return *attributes;
   }
 
+  absl::string_view PlatformName() const override { return "cpu"; }
+
   absl::string_view Kind() const override { return "cpu"; }
 
   absl::string_view ToString() const override { return "NanoRT CPU"; }
