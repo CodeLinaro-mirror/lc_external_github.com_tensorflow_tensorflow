hexagon-geo-surv/tensorflow

Update `rules_ml_toolchain` version.
PiperOrigin-RevId: 843879495

WORKSPACE
@@ -21,10 +21,10 @@ tf_http_archive(
 # Details: https://github.com/google-ml-infra/rules_ml_toolchain
 tf_http_archive(
     name = "rules_ml_toolchain",
-    sha256 = "1a911c79fc734c39538781a7a4672b06aab8354c1ddb985c98e3df78f430bcde",
-    strip_prefix = "rules_ml_toolchain-f13852164b6fe240f8a989a744221a51e0d485cd",
+    sha256 = "b0cd4ce2c8f932fe0953369fcff86d2675302c5c767fd62b4c697bf8b37f0d36",
+    strip_prefix = "rules_ml_toolchain-a005f7efcdb10ceff87b1479586e367e4037a908",
     urls = tf_mirror_urls(
-        "https://github.com/google-ml-infra/rules_ml_toolchain/archive/f13852164b6fe240f8a989a744221a51e0d485cd.tar.gz",
+        "https://github.com/google-ml-infra/rules_ml_toolchain/archive/a005f7efcdb10ceff87b1479586e367e4037a908.tar.gz",
     ),
 )
 

tensorflow/workspace0.bzl
@@ -108,10 +108,10 @@ def workspace():
     # Details: https://github.com/google-ml-infra/rules_ml_toolchain
     tf_http_archive(
         name = "rules_ml_toolchain",
-        sha256 = "b1e5e306d8b1103e73b9b778dfc3a9e069d20664437a03246a235724962b5c94",
-        strip_prefix = "rules_ml_toolchain-484235be45e6843db962c45d08fe4b2b65a6a24c",
+        sha256 = "b0cd4ce2c8f932fe0953369fcff86d2675302c5c767fd62b4c697bf8b37f0d36",
+        strip_prefix = "rules_ml_toolchain-a005f7efcdb10ceff87b1479586e367e4037a908",
         urls = tf_mirror_urls(
-            "https://github.com/google-ml-infra/rules_ml_toolchain/archive/484235be45e6843db962c45d08fe4b2b65a6a24c.tar.gz",
+            "https://github.com/google-ml-infra/rules_ml_toolchain/archive/a005f7efcdb10ceff87b1479586e367e4037a908.tar.gz",
         ),
     )
 

CodeLinaro-mirror/lc_external_github.com_tensorflow_tensorflow

WORKSPACE
@@ -21,10 +21,10 @@ tf_http_archive(
 # Details: https://github.com/google-ml-infra/rules_ml_toolchain
 tf_http_archive(
     name = "rules_ml_toolchain",
-    sha256 = "1a911c79fc734c39538781a7a4672b06aab8354c1ddb985c98e3df78f430bcde",
-    strip_prefix = "rules_ml_toolchain-f13852164b6fe240f8a989a744221a51e0d485cd",
+    sha256 = "e9842de3fefb5a120d3b1647d3a09e6e7071e8df8d1cd2dfe6f66ee31fd2595e",
+    strip_prefix = "rules_ml_toolchain-cb79a8fc8dcf3f75743dcd9b3418a70c884a7269",
     urls = tf_mirror_urls(
-        "https://github.com/google-ml-infra/rules_ml_toolchain/archive/f13852164b6fe240f8a989a744221a51e0d485cd.tar.gz",
+        "https://github.com/google-ml-infra/rules_ml_toolchain/archive/cb79a8fc8dcf3f75743dcd9b3418a70c884a7269.tar.gz",
     ),
 )
 

tensorflow/workspace0.bzl
@@ -108,10 +108,10 @@ def workspace():
     # Details: https://github.com/google-ml-infra/rules_ml_toolchain
     tf_http_archive(
         name = "rules_ml_toolchain",
-        sha256 = "b1e5e306d8b1103e73b9b778dfc3a9e069d20664437a03246a235724962b5c94",
-        strip_prefix = "rules_ml_toolchain-484235be45e6843db962c45d08fe4b2b65a6a24c",
+        sha256 = "e9842de3fefb5a120d3b1647d3a09e6e7071e8df8d1cd2dfe6f66ee31fd2595e",
+        strip_prefix = "rules_ml_toolchain-cb79a8fc8dcf3f75743dcd9b3418a70c884a7269",
         urls = tf_mirror_urls(
-            "https://github.com/google-ml-infra/rules_ml_toolchain/archive/484235be45e6843db962c45d08fe4b2b65a6a24c.tar.gz",
+            "https://github.com/google-ml-infra/rules_ml_toolchain/archive/cb79a8fc8dcf3f75743dcd9b3418a70c884a7269.tar.gz",
         ),
     )
 