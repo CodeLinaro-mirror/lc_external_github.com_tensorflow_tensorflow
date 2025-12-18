openxla/xla

[XLA:GPU]Disable TransposeDimensionGrouper pass and replace it with OTF normalization in emitters
0) Fix a bug (?) in normalization util when normalized dim contains a single dimension
1) Perform normalization OTF for Transpose emitter selection
2) Use normalized shape for unrolling decision in kLoop emitter
3) Use normalized shape to detect slow transposes in triton fusion rewriter

PiperOrigin-RevId: 841759079

xla/service/gpu/transforms/fusion_block_level_rewriter_test.cc
@@ -211,6 +211,38 @@ ENTRY entry  {
   EXPECT_TRUE(HasTritonBlockLevelFusionConfig(root));
 }
 
+TEST_F(FusionBlockLevelRewriterTest,
+       RewritesLoopTransposeFusionWithSplitDimensions) {
+  // This test checks if the rewriter can handle a transpose where dimensions
+  // are split in the HLO but logically contiguous.
+  // Logical shape: [100, 200, 300] -> [300, 200, 100] (Swap dim 0 and 2).
+  // Physical shape: [100, 200, 10, 30] -> [10, 30, 200, 100].
+  // The normalized logical transpose shape should recover the logical swap.
+  const absl::string_view hlo_text = R"(
+fusion_computation {
+  p0 = f32[100,200,10,30] parameter(0)
+  ROOT transpose = f32[10,30,200,100] transpose(p0), dimensions={2,3,1,0}
+}
+
+ENTRY entry {
+  p0 = f32[100,200,10,30] parameter(0)
+  ROOT fusion = f32[10,30,200,100] fusion(p0), kind=kLoop,
+    calls=fusion_computation
+})";
+  TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<HloModule> module,
+                          ParseAndReturnVerifiedModule(hlo_text));
+
+  EXPECT_THAT(
+      FusionBlockLevelRewriter(device_info_, HloCostAnalysis::DefaultShapeSize,
+                               &mlir_context_)
+          .Run(module.get()),
+      absl_testing::IsOkAndHolds(true));
+  const HloInstruction* root = module->entry_computation()->root_instruction();
+  EXPECT_EQ(root->opcode(), HloOpcode::kFusion);
+  EXPECT_EQ(root->fusion_kind(), HloInstruction::FusionKind::kCustom);
+  EXPECT_TRUE(HasTritonBlockLevelFusionConfig(root));
+}
+
 }  // namespace
 }  // namespace gpu
 }  // namespace xla

CodeLinaro-mirror/lc_external_github.com_tensorflow_tensorflow

third_party/xla/xla/service/gpu/transforms/fusion_block_level_rewriter_test.cc
@@ -211,6 +211,38 @@ ENTRY entry  {
   EXPECT_TRUE(HasTritonBlockLevelFusionConfig(root));
 }
 
+TEST_F(FusionBlockLevelRewriterTest,
+       RewritesLoopTransposeFusionWithSplitDimensions) {
+  // This test checks if the rewriter can handle a transpose where dimensions
+  // are split in the HLO but logically contiguous.
+  // Logical shape: [100, 200, 300] -> [300, 200, 100] (Swap dim 0 and 2).
+  // Physical shape: [100, 200, 10, 30] -> [10, 30, 200, 100].
+  // The normalized logical transpose shape should recover the logical swap.
+  const absl::string_view hlo_text = R"(
+fusion_computation {
+  p0 = f32[100,200,10,30] parameter(0)
+  ROOT transpose = f32[10,30,200,100] transpose(p0), dimensions={2,3,1,0}
+}
+
+ENTRY entry {
+  p0 = f32[100,200,10,30] parameter(0)
+  ROOT fusion = f32[10,30,200,100] fusion(p0), kind=kLoop,
+    calls=fusion_computation
+})";
+  ASSERT_OK_AND_ASSIGN(std::unique_ptr<HloModule> module,
+                       ParseAndReturnVerifiedModule(hlo_text));
+
+  EXPECT_THAT(
+      FusionBlockLevelRewriter(device_info_, HloCostAnalysis::DefaultShapeSize,
+                               &mlir_context_)
+          .Run(module.get()),
+      absl_testing::IsOkAndHolds(true));
+  const HloInstruction* root = module->entry_computation()->root_instruction();
+  EXPECT_EQ(root->opcode(), HloOpcode::kFusion);
+  EXPECT_EQ(root->fusion_kind(), HloInstruction::FusionKind::kCustom);
+  EXPECT_TRUE(HasTritonBlockLevelFusionConfig(root));
+}
+
 }  // namespace
 }  // namespace gpu
 }  // namespace xla