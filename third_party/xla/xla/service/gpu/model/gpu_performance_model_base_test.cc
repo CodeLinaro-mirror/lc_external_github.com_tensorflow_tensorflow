ai-jz/tensorflow

[XLA:GPU] Expand device description to include fine-grained execution unit info
We need accurate peak FLOP/s for the GEMM cost model. Both AMD and NVidia have similar distinctions between standard and accelerated computation. Performance characteristics are different per data type.

This change introduces a data structure that can be used to estimate peak ops more precisely. Along with it we add a simple utility function for this estimation and an example usage in tests.

Filling the actual device properties in the executor will be coming in following changes.

PiperOrigin-RevId: 839249186

third_party/xla/xla/service/gpu/model/gpu_performance_model_base_test.cc
@@ -32,7 +32,7 @@ limitations under the License.
 #include "xla/service/gpu/model/gpu_hlo_cost_analysis.h"
 #include "xla/stream_executor/device_description.h"
 #include "xla/tsl/platform/statusor.h"
-#include "tsl/platform/statusor.h"
+#include "xla/xla_data.pb.h"
 
 namespace xla {
 namespace gpu {
@@ -328,6 +328,26 @@ TEST_F(GpuPerformanceModelBaseTest,
       h100_device_info, /*num_blocks=*/h100_device_info.core_count(),
       /*num_threads_per_block=*/h100_device_info.fpus_per_core());
   // H100 has a peak of 66.9 TFLOPS/s for TF32.
+  EXPECT_GT(flops_per_ns, 66000);
+  EXPECT_LT(flops_per_ns, 68000);
+}
+
+TEST_F(GpuPerformanceModelBaseTest, CalculatePeakFP16FlopsPerNsH100) {
+  se::DeviceDescription h100_device_info =
+      TestGpuDeviceInfo::RTXH100SXMDeviceInfo();
+  int64_t flops_per_ns = GpuPerformanceModelBase::CalculatePeakMatrixFlopsPerNs(
+      h100_device_info, xla::PrimitiveType::BF16);
+  // H100 has a peak of 989.4 TFLOPS/s for BF16.
+  EXPECT_GT(flops_per_ns, 988000);
+  EXPECT_LT(flops_per_ns, 991000);
+}
+
+TEST_F(GpuPerformanceModelBaseTest, CalculatePeakFP64FlopsPerNsH100) {
+  se::DeviceDescription h100_device_info =
+      TestGpuDeviceInfo::RTXH100SXMDeviceInfo();
+  int64_t flops_per_ns = GpuPerformanceModelBase::CalculatePeakMatrixFlopsPerNs(
+      h100_device_info, xla::PrimitiveType::F64);
+  // H100 has a peak of 66.8 TFLOPS/s for FP64.
   EXPECT_GT(flops_per_ns, 66000);
   EXPECT_LT(flops_per_ns, 68000);
 }

third_party/xla/xla/stream_executor/device_description_test.cc
@@ -26,6 +26,8 @@ limitations under the License.
 namespace stream_executor {
 namespace {
 using absl_testing::IsOkAndHolds;
+using testing::Eq;
+using testing::Pointee;
 
 TEST(DeviceDescription, DefaultConstruction) {
   DeviceDescription desc;
@@ -38,6 +40,8 @@ TEST(DeviceDescription, DefaultConstruction) {
   EXPECT_EQ(desc.driver_version(), kZeroVersion);
   EXPECT_EQ(desc.runtime_version(), kZeroVersion);
   EXPECT_EQ(desc.pci_bus_id(), "<undefined>");
+  EXPECT_EQ(desc.scalar_unit_description(), nullptr);
+  EXPECT_EQ(desc.matrix_unit_description(), nullptr);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -130,6 +134,36 @@ TEST(GpuComputeCapability, ProtoConversion) {
       GpuComputeCapability::FromProto(
           GpuComputeCapability(RocmComputeCapability("gfx900")).ToProto()),
       IsOkAndHolds(GpuComputeCapability(RocmComputeCapability("gfx900"))));
+}
+
+TEST(ExecutionUnitDescription, ProtoConversion) {
+  ExecutionUnitDescription desc;
+  desc.SetRateInfo(xla::F32, ExecutionUnitDescription::RateInfo{128, 1.5f, 2});
+  desc.SetRateInfo(xla::F16, ExecutionUnitDescription::RateInfo{256, 1.5f, 4});
+
+  EXPECT_THAT(ExecutionUnitDescription::FromProto(desc.ToProto()),
+              IsOkAndHolds(desc));
+}
+
+TEST(DeviceDescription, ProtoConversion) {
+  DeviceDescription desc;
+  ExecutionUnitDescription scalar_unit_desc;
+  scalar_unit_desc.SetRateInfo(xla::F32,
+                               ExecutionUnitDescription::RateInfo{64, 1.2f, 1});
+  desc.set_scalar_unit_description(scalar_unit_desc);
+
+  ExecutionUnitDescription matrix_unit_desc;
+  matrix_unit_desc.SetRateInfo(
+      xla::F16, ExecutionUnitDescription::RateInfo{128, 1.2f, 8});
+  desc.set_matrix_unit_description(matrix_unit_desc);
+
+  ASSERT_OK_AND_ASSIGN(DeviceDescription from_proto,
+                       DeviceDescription::FromProto(desc.ToGpuProto()));
+
+  EXPECT_THAT(from_proto.scalar_unit_description(),
+              Pointee(Eq(*desc.scalar_unit_description())));
+  EXPECT_THAT(from_proto.matrix_unit_description(),
+              Pointee(Eq(*desc.matrix_unit_description())));
 }
 
 }  // namespace

CodeLinaro-mirror/lc_external_github.com_tensorflow_tensorflow

third_party/xla/xla/service/gpu/model/gpu_performance_model_base_test.cc
@@ -31,7 +31,7 @@ limitations under the License.
 #include "xla/service/gpu/model/gpu_hlo_cost_analysis.h"
 #include "xla/stream_executor/device_description.h"
 #include "xla/tsl/platform/statusor.h"
-#include "tsl/platform/statusor.h"
+#include "xla/xla_data.pb.h"
 
 namespace xla {
 namespace gpu {
@@ -327,6 +327,26 @@ TEST_F(GpuPerformanceModelBaseTest,
       h100_device_info, /*num_blocks=*/h100_device_info.core_count(),
       /*num_threads_per_block=*/h100_device_info.fpus_per_core());
   // H100 has a peak of 66.9 TFLOPS/s for TF32.
+  EXPECT_GT(flops_per_ns, 66000);
+  EXPECT_LT(flops_per_ns, 68000);
+}
+
+TEST_F(GpuPerformanceModelBaseTest, CalculatePeakBF16OpsPerNsH100) {
+  se::DeviceDescription h100_device_info =
+      TestGpuDeviceInfo::RTXH100SXMDeviceInfo();
+  int64_t flops_per_ns = GpuPerformanceModelBase::CalculatePeakMatrixOpsPerNs(
+      h100_device_info, xla::PrimitiveType::BF16);
+  // H100 has a peak of 989.4 TFLOPS/s for BF16.
+  EXPECT_GT(flops_per_ns, 988000);
+  EXPECT_LT(flops_per_ns, 991000);
+}
+
+TEST_F(GpuPerformanceModelBaseTest, CalculatePeakF64OpsPerNsH100) {
+  se::DeviceDescription h100_device_info =
+      TestGpuDeviceInfo::RTXH100SXMDeviceInfo();
+  int64_t flops_per_ns = GpuPerformanceModelBase::CalculatePeakMatrixOpsPerNs(
+      h100_device_info, xla::PrimitiveType::F64);
+  // H100 has a peak of 66.8 TFLOPS/s for FP64.
   EXPECT_GT(flops_per_ns, 66000);
   EXPECT_LT(flops_per_ns, 68000);
 }

third_party/xla/xla/stream_executor/device_description_test.cc
@@ -22,10 +22,13 @@ limitations under the License.
 #include "xla/stream_executor/cuda/cuda_compute_capability.h"
 #include "xla/stream_executor/rocm/rocm_compute_capability.h"
 #include "xla/stream_executor/semantic_version.h"
+#include "xla/tsl/platform/statusor.h"
 
 namespace stream_executor {
 namespace {
 using absl_testing::IsOkAndHolds;
+using testing::Eq;
+using testing::Pointee;
 
 TEST(DeviceDescription, DefaultConstruction) {
   DeviceDescription desc;
@@ -38,6 +41,8 @@ TEST(DeviceDescription, DefaultConstruction) {
   EXPECT_EQ(desc.driver_version(), kZeroVersion);
   EXPECT_EQ(desc.runtime_version(), kZeroVersion);
   EXPECT_EQ(desc.pci_bus_id(), "<undefined>");
+  EXPECT_EQ(desc.scalar_unit_description(), nullptr);
+  EXPECT_EQ(desc.matrix_unit_description(), nullptr);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -130,6 +135,36 @@ TEST(GpuComputeCapability, ProtoConversion) {
       GpuComputeCapability::FromProto(
           GpuComputeCapability(RocmComputeCapability("gfx900")).ToProto()),
       IsOkAndHolds(GpuComputeCapability(RocmComputeCapability("gfx900"))));
+}
+
+TEST(ExecutionUnitDescription, ProtoConversion) {
+  ExecutionUnitDescription desc;
+  desc.SetRateInfo(xla::F32, ExecutionUnitDescription::RateInfo{128, 1.5f, 2});
+  desc.SetRateInfo(xla::F16, ExecutionUnitDescription::RateInfo{256, 1.5f, 4});
+
+  EXPECT_THAT(ExecutionUnitDescription::FromProto(desc.ToProto()),
+              IsOkAndHolds(desc));
+}
+
+TEST(DeviceDescription, ProtoConversion) {
+  DeviceDescription desc;
+  ExecutionUnitDescription scalar_unit_desc;
+  scalar_unit_desc.SetRateInfo(xla::F32,
+                               ExecutionUnitDescription::RateInfo{64, 1.2f, 1});
+  desc.set_scalar_unit_description(scalar_unit_desc);
+
+  ExecutionUnitDescription matrix_unit_desc;
+  matrix_unit_desc.SetRateInfo(
+      xla::F16, ExecutionUnitDescription::RateInfo{128, 1.2f, 8});
+  desc.set_matrix_unit_description(matrix_unit_desc);
+
+  TF_ASSERT_OK_AND_ASSIGN(DeviceDescription from_proto,
+                          DeviceDescription::FromProto(desc.ToGpuProto()));
+
+  EXPECT_THAT(from_proto.scalar_unit_description(),
+              Pointee(Eq(*desc.scalar_unit_description())));
+  EXPECT_THAT(from_proto.matrix_unit_description(),
+              Pointee(Eq(*desc.matrix_unit_description())));
 }
 
 }  // namespace