openxla/xla

[XLA][codegen] Migrate binary ops to stablehlo
This is a step towards emitting unsigned integer fusions, which is why we lower to sign specific functions in the shlo lowerings.

This CL migrates: add, mul, div, rem, xor, or, and, max, min

PiperOrigin-RevId: 846675287

xla/codegen/xtile/ir/transforms/verify_legal_xtile_ops.cc
@@ -83,8 +83,12 @@ std::optional<absl::string_view> IsLegalTensorOp(mlir::Operation* op) {
 std::optional<absl::string_view> IsLegalStablehloOp(mlir::Operation* op) {
   if (mlir::isa<mlir::stablehlo::BroadcastInDimOp, mlir::stablehlo::ReduceOp,
                 mlir::stablehlo::ReturnOp, mlir::stablehlo::TransposeOp,
-                mlir::stablehlo::DotGeneralOp, mlir::stablehlo::ReshapeOp>(
-          op)) {
+                mlir::stablehlo::DotGeneralOp, mlir::stablehlo::ReshapeOp,
+                mlir::stablehlo::AddOp, mlir::stablehlo::DivOp,
+                mlir::stablehlo::RemOp, mlir::stablehlo::MaxOp,
+                mlir::stablehlo::MinOp, mlir::stablehlo::MulOp,
+                mlir::stablehlo::SubtractOp, mlir::stablehlo::XorOp,
+                mlir::stablehlo::AndOp, mlir::stablehlo::OrOp>(op)) {
     return std::nullopt;
   }
 

CodeLinaro-mirror/lc_external_github.com_tensorflow_tensorflow

third_party/xla/xla/codegen/xtile/ir/transforms/verify_legal_xtile_ops.cc
@@ -84,7 +84,13 @@ std::optional<absl::string_view> IsLegalStablehloOp(mlir::Operation* op) {
   if (mlir::isa<mlir::stablehlo::BroadcastInDimOp, mlir::stablehlo::ReduceOp,
                 mlir::stablehlo::ReturnOp, mlir::stablehlo::TransposeOp,
                 mlir::stablehlo::DotGeneralOp, mlir::stablehlo::ReshapeOp,
-                mlir::stablehlo::AllReduceOp>(op)) {
+                mlir::stablehlo::DotGeneralOp, mlir::stablehlo::ReshapeOp,
+                mlir::stablehlo::AllReduceOp, mlir::stablehlo::AddOp,
+                mlir::stablehlo::DivOp, mlir::stablehlo::RemOp,
+                mlir::stablehlo::MaxOp, mlir::stablehlo::MinOp,
+                mlir::stablehlo::MulOp, mlir::stablehlo::SubtractOp,
+                mlir::stablehlo::XorOp, mlir::stablehlo::AndOp,
+                mlir::stablehlo::OrOp>(op)) {
     return std::nullopt;
   }
 