39026a7f0cfb7093dbd30604cc2c6efa401dd716
Parker Schuh <parkers@google.com>
2026-02-26 10:42:13 -0800

    Allow testing difference between se_gpu_client async and tfrt_tpu_client_async via an env var.
    PiperOrigin-RevId: 875791988

--- third_party/xla/xla/pjrt/plugin/xla_gpu/xla_gpu_pjrt_client.cc
+++ third_party/xla/xla/pjrt/plugin/xla_gpu/xla_gpu_pjrt_client.cc
@@ -27,7 +27,14 @@ namespace xla {
 
 absl::StatusOr<std::unique_ptr<PjRtClient>> GetXlaPjrtGpuClient(
     GpuClientOptions options) {
-  if (options.use_tfrt_gpu_client) {
+  bool use_tfrt_gpu_client = options.use_tfrt_gpu_client;
+  // Enabled by default.
+  if (const char* v = std::getenv("ENABLE_TFRT_GPU_CLIENT")) {
+    if (absl::string_view(v) == "0") {
+      use_tfrt_gpu_client = false;
+    }
+  }
+  if (use_tfrt_gpu_client) {
     return GetTfrtGpuClient(options);
   }
   return GetStreamExecutorGpuClient(options);
