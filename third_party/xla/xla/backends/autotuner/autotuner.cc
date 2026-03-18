08d8438d3cb73cdca1f73c3893b8cd01ca9472b2
A. Unique TensorFlower <gardener@tensorflow.org>
2026-03-18 13:48:15 -0700

    Add registered codegen backends to KV store key, for multi-host sharding.
    - This ensures that multi-host sharding doesn't break if the codegen-backends registered with the autotuner changes. These can change due to xla_flags or when we change default backends in XLA code.
    
    PiperOrigin-RevId: 885768657

--- third_party/xla/xla/backends/autotuner/autotuner.cc
+++ third_party/xla/xla/backends/autotuner/autotuner.cc
@@ -33,6 +33,7 @@ limitations under the License.
 #include "absl/log/log.h"
 #include "absl/memory/memory.h"
 #include "absl/status/status.h"
+#include "absl/strings/escaping.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
 #include "absl/strings/str_join.h"
@@ -118,9 +119,20 @@ std::string UnpackedAnyShortDebugString(const google::protobuf::Any& any) {
 // client, but with a different module config each time, we may hit the
 // cache the second time and recover potentially inferior, or incomplete
 // autotuning results.
-std::string GetKvStoreKey(const HloModule* module, int shard_index) {
+std::string GetKvStoreKey(
+    const HloModule* module, int shard_index,
+    const std::vector<std::unique_ptr<CodegenBackend>>& codegen_backends) {
+  std::string backend_names = absl::StrJoin(
+      codegen_backends, ",",
+      [](std::string* out, const std::unique_ptr<CodegenBackend>& backend) {
+        absl::StrAppend(out, backend->name());
+      });
+  auto backend_fprint_bytes =
+      tsl::Fprint128ToBytes(tsl::Fingerprint128(backend_names));
+  std::string backend_fingerprint = absl::BytesToHexString(absl::string_view(
+      backend_fprint_bytes.data(), backend_fprint_bytes.size()));
   return absl::StrCat("autotune_results_", module->GetFingerprint128(), "_",
-                      shard_index);
+                      backend_fingerprint, "_", shard_index);
 }
 
 }  // namespace
@@ -231,7 +243,8 @@ absl::Status Autotuner::Autotune(HloModule* module,
 
   // 4. Store the results for this shard as a serialized string to the KV store.
   KeyValueStoreInterface& kv_store = *sharding_kv_store.key_value_store;
-  const std::string local_key = GetKvStoreKey(module, my_shard_index);
+  const std::string local_key =
+      GetKvStoreKey(module, my_shard_index, codegen_backends_);
   std::string local_results;
   if (!autotuned_instructions.empty()) {
     TF_ASSIGN_OR_RETURN(local_results,
@@ -254,7 +267,7 @@ absl::Status Autotuner::Autotune(HloModule* module,
     if (i == my_shard_index) {
       continue;
     }
-    const std::string remote_key = GetKvStoreKey(module, i);
+    const std::string remote_key = GetKvStoreKey(module, i, codegen_backends_);
     VLOG(2) << "Shard " << my_shard_index << ": waiting for results from shard "
             << i << " / " << total_shards << " at " << remote_key;
     // TODO(b/361009609): reset to infinite duration once issue with MPI is
@@ -485,9 +498,11 @@ std::optional<Autotuner::Config> Autotuner::LookUp(
           return Config{codegen_backend.get(), std::move(backend_config)};
         }
       }
-      LOG(WARNING) << "Cached config for HLO: " << instr->ToString()
-                   << " has unsupported backend "
-                   << cached_config->codegen_backend;
+      LOG(WARNING) << "Ignoring cached config from backend "
+                   << Backend_Name(cached_config->codegen_backend)
+                   << " for HLO '" << instr->ToString()
+                   << ", because this backend is not registered with the "
+                      "autotuner.";
     }
   }
   return std::nullopt;
