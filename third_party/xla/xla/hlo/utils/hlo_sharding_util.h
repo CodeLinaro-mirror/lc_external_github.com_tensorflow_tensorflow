adityasai1234/tensorflow

Allow mixing V2 and V3 shardings in utility functions.
The utility functions `IsSubTilingOrEqualSharding`, `MergeShardingIfCompatible`, and `CanReshardWithCollectivePermute` are updated to handle cases where one of the input `HloSharding` objects uses named sharding (V3) and the other does not (V2). Previously, these functions had a CHECK to ensure both shardings were either V2 or V3. Now, if a sharding is V3, it is converted to its equivalent V2 representation before the core logic is applied.

PiperOrigin-RevId: 888349396

third_party/xla/xla/hlo/utils/hlo_sharding_util.h
@@ -104,8 +104,8 @@ HloInstruction* ReverseFormatShape(
 //    must align with `sharding` such that no sub-tile crosses the boundary of a
 //    parent tile.
 bool IsSubTilingOrEqualSharding(const Shape& shape,
-                                const HloSharding& potential_subsharding,
-                                const HloSharding& sharding);
+                                const HloSharding& raw_potential_subsharding,
+                                const HloSharding& raw_sharding);
 
 // Returns true if the lhs sharding is preferable over the rhs sharding.
 // The most specific sharding is tile maximal followed by single device tile
@@ -122,11 +122,12 @@ bool MergeSharding(const HloSharding& to_merge, HloSharding* dst,
 
 // Merges `to_merge` into `dst` only if they are compatible, and the merged
 // sharding has >= `minimum_tiles` tiles. Returns if merging happened.
-bool MergeShardingIfCompatible(const HloSharding& to_merge,
+bool MergeShardingIfCompatible(const HloSharding& raw_to_merge,
                                int64_t minimum_tiles, HloSharding* dst);
 
 // Same as above, but with `minimum_tiles` = `dst->NumTiles() + 1`.
-bool MergeShardingIfCompatible(const HloSharding& to_merge, HloSharding* dst);
+bool MergeShardingIfCompatible(const HloSharding& raw_to_merge,
+                               HloSharding* dst);
 
 // Find a reasonable common sharding for a list of shardings. The reasonable
 // sharding should incur little(the least) amount of total resharding cost when

third_party/xla/xla/service/spmd/spmd_partitioner_util.h
@@ -439,8 +439,8 @@ GetReshardAllToAllSourceTargetDims(const HloSharding& source,
                                    const HloSharding& target);
 
 // Returns whether the resharding can be done via collective-permute.
-bool CanReshardWithCollectivePermute(const HloSharding& source,
-                                     const HloSharding& target);
+bool CanReshardWithCollectivePermute(const HloSharding& raw_source,
+                                     const HloSharding& raw_target);
 
 // Returns a new GroupedSharding that has the same group definition of
 // `reference`.

CodeLinaro-mirror/lc_external_github.com_tensorflow_tensorflow

third_party/xla/xla/hlo/utils/hlo_sharding_util.h
@@ -104,8 +104,8 @@ HloInstruction* ReverseFormatShape(
 //    must align with `sharding` such that no sub-tile crosses the boundary of a
 //    parent tile.
 bool IsSubTilingOrEqualSharding(const Shape& shape,
-                                const HloSharding& potential_subsharding,
-                                const HloSharding& sharding);
+                                const HloSharding& raw_potential_subsharding,
+                                const HloSharding& raw_sharding);
 
 // Returns true if the lhs sharding is preferable over the rhs sharding.
 // The most specific sharding is tile maximal followed by single device tile
@@ -122,11 +122,12 @@ bool MergeSharding(const HloSharding& to_merge, HloSharding* dst,
 
 // Merges `to_merge` into `dst` only if they are compatible, and the merged
 // sharding has >= `minimum_tiles` tiles. Returns if merging happened.
-bool MergeShardingIfCompatible(const HloSharding& to_merge,
+bool MergeShardingIfCompatible(const HloSharding& to_merge_input,
                                int64_t minimum_tiles, HloSharding* dst);
 
 // Same as above, but with `minimum_tiles` = `dst->NumTiles() + 1`.
-bool MergeShardingIfCompatible(const HloSharding& to_merge, HloSharding* dst);
+bool MergeShardingIfCompatible(const HloSharding& to_merge_input,
+                               HloSharding* dst);
 
 // Find a reasonable common sharding for a list of shardings. The reasonable
 // sharding should incur little(the least) amount of total resharding cost when

third_party/xla/xla/service/spmd/spmd_partitioner_util.h
@@ -439,8 +439,8 @@ GetReshardAllToAllSourceTargetDims(const HloSharding& source,
                                    const HloSharding& target);
 
 // Returns whether the resharding can be done via collective-permute.
-bool CanReshardWithCollectivePermute(const HloSharding& source,
-                                     const HloSharding& target);
+bool CanReshardWithCollectivePermute(const HloSharding& source_input,
+                                     const HloSharding& target_input);
 
 // Returns a new GroupedSharding that has the same group definition of
 // `reference`.