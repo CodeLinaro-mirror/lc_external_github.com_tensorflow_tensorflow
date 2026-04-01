de2cdb3e5f3d6b731885748807f13b35a6e2980c
Hyeontaek Lim <hyeontaek@google.com>
2026-03-31 17:22:23 -0700

    [IFRT] Add `Value::ByteSize` method
    `xla::ifrt::Value::ByteSize()` is a new API that asks the IFRT runtime to compute the byte size of the IFRT value object (an array or an upcoming tuple). This API will provide the user with a fast and accurate way to calculate on-device sizes of IFRT value objects, and the runtime would be responsible for providing a robust implementation of this calculation.
    
    PiperOrigin-RevId: 892623108

--- third_party/xla/xla/python/ifrt/layout.h
+++ third_party/xla/xla/python/ifrt/layout.h
@@ -78,6 +78,16 @@ class Layout : public llvm::RTTIExtends<Layout, Serializable> {
   virtual absl::StatusOr<std::optional<int64_t>> ByteSize(
       DType dtype, const Shape& shard_shape) const = 0;
 
+  // Computes the byte size of a shard shape using the layout. If `dtype`
+  // represents non-fixed-size (e.g., `kString`), size-less (e.g., `kToken`), or
+  // opaque (`kOpaque`) data, or there is no single fixed size across shards,
+  // returns `std::nullopt`.
+  //
+  // `layout` may be `nullptr`, which represents a default layout.
+  static absl::StatusOr<std::optional<int64_t>> ByteSize(
+      DType dtype, const Shape& shape, const ShardingRef& sharding,
+      absl_nullable const LayoutRef& layout);
+
   // Constructs `Layout` from `LayoutProto`.
   static absl::StatusOr<CustomLayoutRef> FromProto(const LayoutProto& proto);
 
