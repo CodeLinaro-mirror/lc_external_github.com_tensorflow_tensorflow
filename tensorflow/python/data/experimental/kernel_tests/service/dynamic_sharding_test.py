8bcf9a75ceadccd2ba2fd5e3f8849d596413ffe3
Yang Chen <yangchen@google.com>
2026-04-07 14:29:56 -0700

    Add a unit test to make sure tf.data service works with `take`.
    PiperOrigin-RevId: 896089106

--- tensorflow/python/data/experimental/kernel_tests/service/dynamic_sharding_test.py
+++ tensorflow/python/data/experimental/kernel_tests/service/dynamic_sharding_test.py
@@ -382,6 +382,25 @@ class DynamicShardingTest(data_service_test_base.TestBase,
 
   @combinations.generate(
       combinations.times(test_base.default_test_combinations(),
+                         combinations.combine(num_workers=[1, 3])))
+  def testTake(self, num_workers):
+    cluster = data_service_test_base.TestCluster(num_workers=num_workers)
+    a = dataset_ops.Dataset.range(3).repeat().take(5)
+    b = dataset_ops.Dataset.range(100, 103).repeat().take(5)
+    ds = a.concatenate(b)
+    ds = self._make_dynamic_sharding_dataset(ds, cluster)
+
+    assert_items_equal = num_workers > 1
+    if num_workers == 1:
+      expected = [0, 1, 2, 0, 1, 100, 101, 102, 100, 101]
+    else:
+      expected = [0, 1, 2] * 5 + [100, 101, 102] * 5
+    self.assertDatasetProduces(
+        ds, expected, assert_items_equal=assert_items_equal
+    )
+
+  @combinations.generate(
+      combinations.times(test_base.default_test_combinations(),
                          combinations.combine(already_written=[True, False])))
   def testSnapshot(self, already_written):
     num_workers = 3
