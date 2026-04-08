CodeLinaro-mirror/yocto-mirrors_github_tensorflow_tensorflow

Allow Gather, SegmentReduction, and FillEmptyRows GPU Ops to handle `int64` work element counts safely.
This CL introduces `GpuLaunchConfig64` and updates `GetGpuLaunchConfig64` to return `absl::StatusOr<GpuLaunchConfig64>` to safely handle 64-bit work element counts and propagate errors without crashing. The `gather`, `segment_reduction`, and `fill_empty_rows` ops have been updated to use these new 64-bit utilities and handle the status returns.

Additionally, this CL replaces deprecated 1D grid iterators with the new ones that support `auto` or `int64` type loop variables to prevent truncation issues when iterating over large grids.

PiperOrigin-RevId: 810087127

tensorflow/core/kernels/fill_empty_rows_functor_gpu.cu.cc
@@ -54,9 +54,10 @@ typename T::ConstPointerType to_pointers(const T& x) {
 template <typename Tindex, typename... CallerArgs, typename... KernelArgs>
 Status wrap_kernel_call(void (*func)(KernelArgs...), const GPUDevice& device,
                         Tindex size, CallerArgs... args) {
-  auto config = GetGpuLaunchConfig(size, device);
-  return GpuLaunchKernel(func, config.block_count, config.thread_per_block, 0,
-                         device.stream(), config, to_pointers(args)...);
+  auto config = GetGpuLaunchConfig64(size, device);
+  if (!config.ok()) return config.status();
+  return GpuLaunchKernel(func, config->block_count, config->thread_per_block, 0,
+                         device.stream(), *config, to_pointers(args)...);
 }
 };  // namespace kernel_forward
 
@@ -77,10 +78,10 @@ struct CastFunctor {
 // true if the indices are not ordered by row.
 template <typename Tindex>
 __global__ __launch_bounds__(1024) void CountElementsPerRowKernel(
-    GpuLaunchConfig cfg, Tindex dense_rows, int rank, const Tindex* indices,
+    GpuLaunchConfig64 cfg, Tindex dense_rows, int rank, const Tindex* indices,
     Tindex* elements_per_row, int* rows_are_not_ordered,
     int* first_invalid_index) {
-  GPU_1D_KERNEL_LOOP(i, cfg.virtual_thread_count) {
+  for (auto i : GpuGridRangeX(cfg.virtual_thread_count)) {
     Tindex row = indices[i * rank];
     if (row < 0 || row >= dense_rows) {
       GpuAtomicMin(first_invalid_index, i);
@@ -98,8 +99,9 @@ __global__ __launch_bounds__(1024) void CountElementsPerRowKernel(
 
 template <typename Tindex>
 __global__ __launch_bounds__(1024) void CopyRowIndicesKernel(
-    GpuLaunchConfig cfg, int rank, const Tindex* indices, Tindex* row_indices) {
-  GPU_1D_KERNEL_LOOP(i, cfg.virtual_thread_count) {
+    GpuLaunchConfig64 cfg, int rank, const Tindex* indices,
+    Tindex* row_indices) {
+  for (auto i : GpuGridRangeX(cfg.virtual_thread_count)) {
     row_indices[i] = indices[i * rank];
   }
 }
@@ -107,9 +109,9 @@ __global__ __launch_bounds__(1024) void CopyRowIndicesKernel(
 // Sets empty_row_indicator[row] to whether the row is empty.
 template <typename Tindex>
 __global__ __launch_bounds__(1024) void ComputeEmptyRowIndicatorKernel(
-    GpuLaunchConfig cfg, const Tindex* elements_per_row,
+    GpuLaunchConfig64 cfg, const Tindex* elements_per_row,
     bool* empty_row_indicator) {
-  GPU_1D_KERNEL_LOOP(row, cfg.virtual_thread_count) {
+  for (auto row : GpuGridRangeX(cfg.virtual_thread_count)) {
     empty_row_indicator[row] = elements_per_row[row] == 0;
   }
 }
@@ -119,11 +121,11 @@ __global__ __launch_bounds__(1024) void ComputeEmptyRowIndicatorKernel(
 // empty row.
 template <typename T, typename Tindex>
 __global__ __launch_bounds__(1024) void ScatterInputElementsKernel(
-    GpuLaunchConfig cfg, Tindex dense_rows, int rank,
+    GpuLaunchConfig64 cfg, Tindex dense_rows, int rank,
     const Tindex* input_index_map, const Tindex* indices, const T* values,
     const Tindex* num_new_rows_before, Tindex* output_indices, T* output_values,
     Tindex* reverse_index_map) {
-  GPU_1D_KERNEL_LOOP(i, cfg.virtual_thread_count) {
+  for (int64 i : ::tensorflow::GpuGridRangeX(cfg.virtual_thread_count)) {
     Tindex input_i = input_index_map ? input_index_map[i] : i;
     Tindex row = indices[input_i * rank];
     Tindex output_i = i + num_new_rows_before[row];
@@ -141,10 +143,10 @@ __global__ __launch_bounds__(1024) void ScatterInputElementsKernel(
 // input) in output_indices and output_values.
 template <typename T, typename Tindex>
 __global__ __launch_bounds__(1024) void ScatterNewElementsKernel(
-    GpuLaunchConfig cfg, int rank, const T* default_value,
+    GpuLaunchConfig64 cfg, int rank, const T* default_value,
     const Tindex* num_new_rows_through, const Tindex* input_row_ends,
     const bool* empty_row_indicator, Tindex* output_indices, T* output_values) {
-  GPU_1D_KERNEL_LOOP(row, cfg.virtual_thread_count) {
+  for (int64 row : ::tensorflow::GpuGridRangeX(cfg.virtual_thread_count)) {
     if (!empty_row_indicator[row]) continue;  // Only process empty rows
     Tindex input_i = (row == 0 ? 0 : input_row_ends[row - 1]);
     Tindex output_i = input_i + (row == 0 ? 0 : num_new_rows_through[row - 1]);
@@ -489,9 +491,9 @@ namespace {
 
 template <typename T, typename Tindex>
 __global__ __launch_bounds__(1024) void GatherOriginalGradValuesKernel(
-    GpuLaunchConfig cfg, const Tindex* reverse_index_map, const T* grad_values,
-    T* d_values, bool* visited, Tindex N_full) {
-  GPU_1D_KERNEL_LOOP(input_i, cfg.virtual_thread_count) {
+    GpuLaunchConfig64 cfg, const Tindex* reverse_index_map,
+    const T* grad_values, T* d_values, bool* visited, Tindex N_full) {
+  for (auto input_i : GpuGridRangeX(cfg.virtual_thread_count)) {
     Tindex output_i = reverse_index_map[input_i];
     if (output_i >= 0 && output_i < N_full) {
       d_values[input_i] = grad_values[output_i];

tensorflow/core/util/gpu_device_functions.h
@@ -161,8 +161,8 @@ using CudaGridRange = GpuGridRange<T...>;
 template <typename T>
 __device__ detail::GpuGridRange<T> GpuGridRangeX(T count) {
   return detail::GpuGridRange<T>(
-      /*begin=*/blockIdx.x * blockDim.x + threadIdx.x,
-      /*delta=*/gridDim.x * blockDim.x, /*end=*/count);
+      /*begin=*/static_cast<T>(blockIdx.x) * blockDim.x + threadIdx.x,
+      /*delta=*/static_cast<T>(gridDim.x) * blockDim.x, /*end=*/count);
 }
 CREATE_CUDA_DEVICE_FUNCTION_ALIAS(GpuGridRangeX, CudaGridRangeX);
 
@@ -171,8 +171,8 @@ CREATE_CUDA_DEVICE_FUNCTION_ALIAS(GpuGridRangeX, CudaGridRangeX);
 template <typename T>
 __device__ detail::GpuGridRange<T> GpuGridRangeY(T count) {
   return detail::GpuGridRange<T>(
-      /*begin=*/blockIdx.y * blockDim.y + threadIdx.y,
-      /*delta=*/gridDim.y * blockDim.y, /*end=*/count);
+      /*begin=*/static_cast<T>(blockIdx.y) * blockDim.y + threadIdx.y,
+      /*delta=*/static_cast<T>(gridDim.y) * blockDim.y, /*end=*/count);
 }
 CREATE_CUDA_DEVICE_FUNCTION_ALIAS(GpuGridRangeY, CudaGridRangeY);
 
@@ -181,8 +181,8 @@ CREATE_CUDA_DEVICE_FUNCTION_ALIAS(GpuGridRangeY, CudaGridRangeY);
 template <typename T>
 __device__ detail::GpuGridRange<T> GpuGridRangeZ(T count) {
   return detail::GpuGridRange<T>(
-      /*begin=*/blockIdx.z * blockDim.z + threadIdx.z,
-      /*delta=*/gridDim.z * blockDim.z, /*end=*/count);
+      /*begin=*/static_cast<T>(blockIdx.z) * blockDim.z + threadIdx.z,
+      /*delta=*/static_cast<T>(gridDim.z) * blockDim.z, /*end=*/count);
 }
 CREATE_CUDA_DEVICE_FUNCTION_ALIAS(GpuGridRangeZ, CudaGridRangeZ);
 
@@ -491,24 +491,25 @@ CREATE_CUDA_DEVICE_FUNCTION_ALIAS(GpuLdg, CudaLdg);
 // Note: this function does not synchronize, and therefore the memory range is
 // not guaranteed to be zero until the next kernel launch.
 template <typename T>
-__global__ void SetZero(const int count, T* __restrict__ ptr) {
+__global__ void SetZero(const int64_t count, T* __restrict__ ptr) {
   // Check that the grid is one dimensional and index doesn't overflow.
   assert(blockDim.y == 1);
   assert(blockDim.z == 1);
   assert(blockDim.x * gridDim.x / blockDim.x == gridDim.x);
-  for (int i : GpuGridRangeX(count)) {
+  for (auto i : GpuGridRangeX(count)) {
     ptr[i] = T(0);
   }
 }
 
 // Helper to set all tensor entries to a specific value.
 template <typename T, typename Tvalue = T>
-__global__ void SetToValue(const int count, T* __restrict__ ptr, Tvalue value) {
+__global__ void SetToValue(const int64_t count, T* __restrict__ ptr,
+                           Tvalue value) {
   // Check that the grid is one dimensional and index doesn't overflow.
   assert(blockDim.y == 1);
   assert(blockDim.z == 1);
   assert(blockDim.x * gridDim.x / blockDim.x == gridDim.x);
-  for (int i : GpuGridRangeX(count)) {
+  for (auto i : GpuGridRangeX(count)) {
     ptr[i] = static_cast<T>(value);
   }
 }

CodeLinaro-mirror/lc_external_github.com_tensorflow_tensorflow

tensorflow/core/kernels/fill_empty_rows_functor_gpu.cu.cc
@@ -54,9 +54,10 @@ typename T::ConstPointerType to_pointers(const T& x) {
 template <typename Tindex, typename... CallerArgs, typename... KernelArgs>
 Status wrap_kernel_call(void (*func)(KernelArgs...), const GPUDevice& device,
                         Tindex size, CallerArgs... args) {
-  auto config = GetGpuLaunchConfig(size, device);
-  return GpuLaunchKernel(func, config.block_count, config.thread_per_block, 0,
-                         device.stream(), config, to_pointers(args)...);
+  StatusOr<GpuLaunchConfig64> config = GetGpuLaunchConfig64(size, device);
+  if (!config.ok()) return config.status();
+  return GpuLaunchKernel(func, config->block_count, config->thread_per_block, 0,
+                         device.stream(), *config, to_pointers(args)...);
 }
 };  // namespace kernel_forward
 
@@ -77,10 +78,10 @@ struct CastFunctor {
 // true if the indices are not ordered by row.
 template <typename Tindex>
 __global__ __launch_bounds__(1024) void CountElementsPerRowKernel(
-    GpuLaunchConfig cfg, Tindex dense_rows, int rank, const Tindex* indices,
+    GpuLaunchConfig64 cfg, Tindex dense_rows, int rank, const Tindex* indices,
     Tindex* elements_per_row, int* rows_are_not_ordered,
     int* first_invalid_index) {
-  GPU_1D_KERNEL_LOOP(i, cfg.virtual_thread_count) {
+  for (int64_t i : GpuGridRangeX(cfg.virtual_thread_count)) {
     Tindex row = indices[i * rank];
     if (row < 0 || row >= dense_rows) {
       GpuAtomicMin(first_invalid_index, i);
@@ -98,8 +99,9 @@ __global__ __launch_bounds__(1024) void CountElementsPerRowKernel(
 
 template <typename Tindex>
 __global__ __launch_bounds__(1024) void CopyRowIndicesKernel(
-    GpuLaunchConfig cfg, int rank, const Tindex* indices, Tindex* row_indices) {
-  GPU_1D_KERNEL_LOOP(i, cfg.virtual_thread_count) {
+    GpuLaunchConfig64 cfg, int rank, const Tindex* indices,
+    Tindex* row_indices) {
+  for (int64_t i : GpuGridRangeX(cfg.virtual_thread_count)) {
     row_indices[i] = indices[i * rank];
   }
 }
@@ -107,9 +109,9 @@ __global__ __launch_bounds__(1024) void CopyRowIndicesKernel(
 // Sets empty_row_indicator[row] to whether the row is empty.
 template <typename Tindex>
 __global__ __launch_bounds__(1024) void ComputeEmptyRowIndicatorKernel(
-    GpuLaunchConfig cfg, const Tindex* elements_per_row,
+    GpuLaunchConfig64 cfg, const Tindex* elements_per_row,
     bool* empty_row_indicator) {
-  GPU_1D_KERNEL_LOOP(row, cfg.virtual_thread_count) {
+  for (int64_t row : GpuGridRangeX(cfg.virtual_thread_count)) {
     empty_row_indicator[row] = elements_per_row[row] == 0;
   }
 }
@@ -119,11 +121,11 @@ __global__ __launch_bounds__(1024) void ComputeEmptyRowIndicatorKernel(
 // empty row.
 template <typename T, typename Tindex>
 __global__ __launch_bounds__(1024) void ScatterInputElementsKernel(
-    GpuLaunchConfig cfg, Tindex dense_rows, int rank,
+    GpuLaunchConfig64 cfg, Tindex dense_rows, int rank,
     const Tindex* input_index_map, const Tindex* indices, const T* values,
     const Tindex* num_new_rows_before, Tindex* output_indices, T* output_values,
     Tindex* reverse_index_map) {
-  GPU_1D_KERNEL_LOOP(i, cfg.virtual_thread_count) {
+  for (int64_t i : ::tensorflow::GpuGridRangeX(cfg.virtual_thread_count)) {
     Tindex input_i = input_index_map ? input_index_map[i] : i;
     Tindex row = indices[input_i * rank];
     Tindex output_i = i + num_new_rows_before[row];
@@ -141,10 +143,10 @@ __global__ __launch_bounds__(1024) void ScatterInputElementsKernel(
 // input) in output_indices and output_values.
 template <typename T, typename Tindex>
 __global__ __launch_bounds__(1024) void ScatterNewElementsKernel(
-    GpuLaunchConfig cfg, int rank, const T* default_value,
+    GpuLaunchConfig64 cfg, int rank, const T* default_value,
     const Tindex* num_new_rows_through, const Tindex* input_row_ends,
     const bool* empty_row_indicator, Tindex* output_indices, T* output_values) {
-  GPU_1D_KERNEL_LOOP(row, cfg.virtual_thread_count) {
+  for (int64_t row : ::tensorflow::GpuGridRangeX(cfg.virtual_thread_count)) {
     if (!empty_row_indicator[row]) continue;  // Only process empty rows
     Tindex input_i = (row == 0 ? 0 : input_row_ends[row - 1]);
     Tindex output_i = input_i + (row == 0 ? 0 : num_new_rows_through[row - 1]);
@@ -489,9 +491,9 @@ namespace {
 
 template <typename T, typename Tindex>
 __global__ __launch_bounds__(1024) void GatherOriginalGradValuesKernel(
-    GpuLaunchConfig cfg, const Tindex* reverse_index_map, const T* grad_values,
-    T* d_values, bool* visited, Tindex N_full) {
-  GPU_1D_KERNEL_LOOP(input_i, cfg.virtual_thread_count) {
+    GpuLaunchConfig64 cfg, const Tindex* reverse_index_map,
+    const T* grad_values, T* d_values, bool* visited, Tindex N_full) {
+  for (int64_t input_i : GpuGridRangeX(cfg.virtual_thread_count)) {
     Tindex output_i = reverse_index_map[input_i];
     if (output_i >= 0 && output_i < N_full) {
       d_values[input_i] = grad_values[output_i];

tensorflow/core/util/gpu_device_functions.h
@@ -161,8 +161,8 @@ using CudaGridRange = GpuGridRange<T...>;
 template <typename T>
 __device__ detail::GpuGridRange<T> GpuGridRangeX(T count) {
   return detail::GpuGridRange<T>(
-      /*begin=*/blockIdx.x * blockDim.x + threadIdx.x,
-      /*delta=*/gridDim.x * blockDim.x, /*end=*/count);
+      /*begin=*/static_cast<T>(blockIdx.x) * blockDim.x + threadIdx.x,
+      /*delta=*/static_cast<T>(gridDim.x) * blockDim.x, /*end=*/count);
 }
 CREATE_CUDA_DEVICE_FUNCTION_ALIAS(GpuGridRangeX, CudaGridRangeX);
 
@@ -171,8 +171,8 @@ CREATE_CUDA_DEVICE_FUNCTION_ALIAS(GpuGridRangeX, CudaGridRangeX);
 template <typename T>
 __device__ detail::GpuGridRange<T> GpuGridRangeY(T count) {
   return detail::GpuGridRange<T>(
-      /*begin=*/blockIdx.y * blockDim.y + threadIdx.y,
-      /*delta=*/gridDim.y * blockDim.y, /*end=*/count);
+      /*begin=*/static_cast<T>(blockIdx.y) * blockDim.y + threadIdx.y,
+      /*delta=*/static_cast<T>(gridDim.y) * blockDim.y, /*end=*/count);
 }
 CREATE_CUDA_DEVICE_FUNCTION_ALIAS(GpuGridRangeY, CudaGridRangeY);
 
@@ -181,8 +181,8 @@ CREATE_CUDA_DEVICE_FUNCTION_ALIAS(GpuGridRangeY, CudaGridRangeY);
 template <typename T>
 __device__ detail::GpuGridRange<T> GpuGridRangeZ(T count) {
   return detail::GpuGridRange<T>(
-      /*begin=*/blockIdx.z * blockDim.z + threadIdx.z,
-      /*delta=*/gridDim.z * blockDim.z, /*end=*/count);
+      /*begin=*/static_cast<T>(blockIdx.z) * blockDim.z + threadIdx.z,
+      /*delta=*/static_cast<T>(gridDim.z) * blockDim.z, /*end=*/count);
 }
 CREATE_CUDA_DEVICE_FUNCTION_ALIAS(GpuGridRangeZ, CudaGridRangeZ);
 
@@ -491,24 +491,25 @@ CREATE_CUDA_DEVICE_FUNCTION_ALIAS(GpuLdg, CudaLdg);
 // Note: this function does not synchronize, and therefore the memory range is
 // not guaranteed to be zero until the next kernel launch.
 template <typename T>
-__global__ void SetZero(const int count, T* __restrict__ ptr) {
+__global__ void SetZero(const int64_t count, T* __restrict__ ptr) {
   // Check that the grid is one dimensional and index doesn't overflow.
   assert(blockDim.y == 1);
   assert(blockDim.z == 1);
   assert(blockDim.x * gridDim.x / blockDim.x == gridDim.x);
-  for (int i : GpuGridRangeX(count)) {
+  for (int64_t i : GpuGridRangeX(count)) {
     ptr[i] = T(0);
   }
 }
 
 // Helper to set all tensor entries to a specific value.
 template <typename T, typename Tvalue = T>
-__global__ void SetToValue(const int count, T* __restrict__ ptr, Tvalue value) {
+__global__ void SetToValue(const int64_t count, T* __restrict__ ptr,
+                           Tvalue value) {
   // Check that the grid is one dimensional and index doesn't overflow.
   assert(blockDim.y == 1);
   assert(blockDim.z == 1);
   assert(blockDim.x * gridDim.x / blockDim.x == gridDim.x);
-  for (int i : GpuGridRangeX(count)) {
+  for (int64_t i : GpuGridRangeX(count)) {
     ptr[i] = static_cast<T>(value);
   }
 }