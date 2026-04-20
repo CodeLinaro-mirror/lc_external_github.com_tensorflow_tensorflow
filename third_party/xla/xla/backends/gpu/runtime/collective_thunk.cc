fb0d1dd86e380090a0233a9c6da413454a3a2afc
Levon Ter-Grigoryan <patrios@google.com>
2026-04-20 09:23:25 -0700

    [XLA:GPU] Move buffers and buffer_uses to CollectiveThunk base class.
    This is a first step towards symmetric memory usage for symmetric NCCL kernels.
    
    PiperOrigin-RevId: 902673687

--- third_party/xla/xla/backends/gpu/runtime/collective_thunk.cc
+++ third_party/xla/xla/backends/gpu/runtime/collective_thunk.cc
@@ -202,8 +202,11 @@ CollectiveConfig GetCollectiveConfig(
 }
 
 CollectiveThunk::CollectiveThunk(Kind kind, ThunkInfo thunk_info,
+                                 std::vector<Buffer> buffers,
                                  CommunicationId communication_id)
-    : Thunk(kind, thunk_info), communication_id_(communication_id) {}
+    : Thunk(kind, thunk_info),
+      buffers_(std::move(buffers)),
+      communication_id_(communication_id) {}
 
 absl::StatusOr<GpuCliqueKey> GetCollectiveGpuCliqueKey(
     const CollectiveParams& params, const CollectiveConfig& collective_config,
