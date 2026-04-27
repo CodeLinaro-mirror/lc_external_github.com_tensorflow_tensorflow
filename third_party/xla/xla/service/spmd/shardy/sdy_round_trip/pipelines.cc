675f11c5817e28bf4f9cc62e344dd05ca503270b
Enver Kayaaslan <enver@google.com>
2026-04-27 05:11:17 -0700

    Push shardy flattener down past OpenWhileFreeVarsSharding pass.
    OpenWhileFreeVarsShardingPass does not impact or get impacted on the call graph. It manipulates stablehlo while ops and their context (within func boundaries)
    
    PiperOrigin-RevId: 906291410

--- third_party/xla/xla/service/spmd/shardy/sdy_round_trip/pipelines.cc
+++ third_party/xla/xla/service/spmd/shardy/sdy_round_trip/pipelines.cc
@@ -90,10 +90,10 @@ void addSdyRoundTripImportPipeline(mlir::OpPassManager& pm,
   pm.addNestedPass<FuncOp>(
       mlir::stablehlo_ext::createStablehloCanonicalizeFromHloImportPass());
   pm.addPass(createSdyRoundTripImportShardyAttrsPass(enableHloShardingV3));
-  pm.addPass(mlir::sdy::createFlattenCallGraphPass());
   pm.addPass(createSdyRoundTripShardMapImportPass());
   pm.addPass(createImportSdyCustomCallsPass());
   pm.addNestedPass<FuncOp>(createOpenWhileFreeVarsShardingPass());
+  pm.addPass(mlir::sdy::createFlattenCallGraphPass());
   if (enableHloShardingV3 || liftAndDedupMeshes) {
     // Lift and dedup inlined meshes in case of HloShardingV3 as meshes are:
     // * Inlined in HloShardingV3 and the converted sdy shardings.
